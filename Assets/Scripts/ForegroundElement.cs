e08adb4bc14285253c16473ad32c25d4a5f22fb6
Guilherme de Deus <kingbowserii@gmail.com>
2020-07-22 20:35:29 -0300

    Movimento de elementos no campo da nave

---
+++ Assets/Scripts/ForegroundElement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundElement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float playerSpeed = StageController.controller.GetPlayerShipSpeed();
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - playerSpeed,
+                                                                          transform.position.y, transform.position.z), Time.deltaTime);
+    }
+}
