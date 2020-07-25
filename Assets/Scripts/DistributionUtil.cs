e2be6313af56645b57ff73800dfce05078ac02a4
Guilherme de Deus <kingbowserii@gmail.com>
2020-07-25 20:11:02 -0300

    Permitindo a utilização de uma geração de distribuição normal

---
+++ Assets/Scripts/DistributionUtil.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistributionUtil : MonoBehaviour
+{
+    public static float GetNormalDistribution(float minValue, float maxValue) {
+        float averageValue = (minValue + maxValue) / 2;
+        return Random.Range(0, averageValue - minValue) + Random.Range(0, averageValue - minValue) + minValue;
+    }
+}
