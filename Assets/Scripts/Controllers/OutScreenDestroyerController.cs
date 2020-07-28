aefcf3f2f80b80f5bc0c2df35b8a1e9e2a297056
Guilherme de Deus <kingbowserii@gmail.com>
2020-07-28 15:15:31 -0300

    Tratando lista de destruiveis a cada Update

--- Assets/Scripts/Controllers/OutScreenDestroyerController.cs
+++ Assets/Scripts/Controllers/OutScreenDestroyerController.cs
@@ -45,72 +45,42 @@ public class OutScreenDestroyerController : MonoBehaviour {
 		/*
 		 * Pick one object at a serial order
 		 */
-        // Test if current index can be picked
-        if (currentObjectIndex < Mathf.FloorToInt(destructibleObjectsList.Count * currentLimit)) {
-            //if (currentObjectIndex < Mathf.FloorToInt(destructibleObjectsList.Count * 0.85f)) {
-            //if (currentObjectIndex < Mathf.FloorToInt(destructibleObjectsList.Count)) {
-            GameObject curDestructible = destructibleObjectsList[currentObjectIndex];
-			//			Debug.Log("index: " + currentObjectIndex + " size: " + destructibleObjectsList.Count);
+		// Test if current index can be picked
+		while (true) {
+			if (currentObjectIndex < Mathf.FloorToInt(destructibleObjectsList.Count * currentLimit)) {
+				GameObject curDestructible = destructibleObjectsList[currentObjectIndex];
+				//			Debug.Log("index: " + currentObjectIndex + " size: " + destructibleObjectsList.Count);
 
-			if (ObjectCrossedCameraXBound(curDestructible)) {
-				destructibleObjectsList.Remove(curDestructible);
-				Destroy(curDestructible);
-				averageDestroyTries = ((averageDestroyTries * randomDestroys) + randomTries) / (randomDestroys + 1);
-				//				Debug.Log("Destroyed " + currentObjectIndex + " of " + destructibleObjectsList.Count + " with " + randomTries + " tries, average is " + averageDestroyTries + " tries");
-				randomTries = 1;
-				randomDestroys++;
+				if (ObjectCrossedCameraXBound(curDestructible)) {
+					destructibleObjectsList.Remove(curDestructible);
+					Destroy(curDestructible);
+					averageDestroyTries = ((averageDestroyTries * randomDestroys) + randomTries) / (randomDestroys + 1);
+					//				Debug.Log("Destroyed " + currentObjectIndex + " of " + destructibleObjectsList.Count + " with " + randomTries + " tries, average is " + averageDestroyTries + " tries");
+					randomTries = 1;
+					randomDestroys++;
+				}
+				else {
+					currentObjectIndex++;
+					randomTries++;
+				}
 			}
+			// If can't, return to starting index
 			else {
-				currentObjectIndex++;
-				randomTries++;
-			}
+				currentObjectIndex = 0;
+
+				// Set new limit
+				currentLimit = Random.Range(0.25f, 1.1f);
+				if (currentLimit > 1) {
+					currentLimit = 1;
+				}
+                break;
+            }
 		}
-		// If can't, return to starting index
-		else {
-			//if (Random.Range(0, 100) < 10) {
-			//    currentObjectIndex = 0;
-			//}
-			//else {
-			//    currentObjectIndex = Mathf.FloorToInt(destructibleObjectsList.Count * 0.15f);
-			//}
-			currentObjectIndex = 0;
-
-			// Set new limit
-			currentLimit = Random.Range(0.25f, 1.1f);
-			if (currentLimit > 1) {
-				currentLimit = 1;
-			}
-			//break;
-		}
-
-		/*
-		 * Pick one object at random
-		 */
-		//if (destructibleObjectsList.Count > 0) {
-		//	int randIndex = Random.Range(0, destructibleObjectsList.Count);
-		//	GameObject curDestructible = destructibleObjectsList[randIndex];
-		//	//			Debug.Log("At index " + randIndex);
-
-		//	if (curDestructible.GetComponent<SpriteRenderer>().sprite.bounds.extents.x + curDestructible.transform.position.x
-		//	    < Camera.main.ScreenToWorldPoint(Vector3.zero).x) {
-		//		destructibleObjectsList.Remove(curDestructible);
-		//		Destroy(curDestructible);
-		//	}
-		//}
 
 		objCount = destructibleObjectsList.Count;
 	}
 	
 	public void AddToDestructibleList(GameObject gameObject) {
-		//string speeds = "";
-
-		//for (int index = 0; index < destructibleObjectsList.Count; index++) {
-
-		//	GameObject currentObj = destructibleObjectsList[index];
-		//	speeds += (currentObj.GetComponent<DestructibleObject>().GetSpeed().ToString() + ";");
-		//}
-		//Debug.Log(speeds);
-
 		// Check speeds to create order
 		for (int index = 0; index < destructibleObjectsList.Count; index++) {
 			GameObject currentObj = destructibleObjectsList[index];
