c9e55f5a523c5f4caf12d7eaffa2a8042551b435
Guilherme de Deus <kingbowserii@gmail.com>
2020-09-10 23:40:42 -0300

    Adding TODO reminder

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -326,6 +326,7 @@ public class BlockSpawner : MonoBehaviour {
 	}
 
 	private float GetGameObjectVerticalSize(GameObject gameObj) {
+		// TODO find a way to get the object's largest sprite
 		return gameObj.GetComponent<SpriteRenderer>().sprite.bounds.extents.y * 2 * gameObj.transform.localScale.y;
 	}
 
