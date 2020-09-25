e1c6a8569f31d54f99d53f4909d74b44aed338a7
Guilherme de Deus <kingbowserii@gmail.com>
2020-09-25 20:23:39 -0300

    Spaceship turning vertically is smoother

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	private const float VERTICAL_SPEED = 2.5f;
+	private const float MAX_TURNING_ANGLE = 0.06f;
+	private const float TURNING_SPEED = 2.5f;
 
 	float blockPosition = 0;
 
@@ -24,14 +26,13 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate() {
         blockBallPosition.position = Vector3.Lerp(blockBallPosition.position, new Vector3(blockBallPosition.position.x, blockPosition, 0), VERTICAL_SPEED * Time.deltaTime);
-		Debug.Log(player.position.y - blockPosition);
 		if (Mathf.Abs(player.position.y - blockPosition) > 0.1f) {
-			player.rotation = new Quaternion(0, 0, Mathf.Lerp(player.rotation.z, Mathf.Clamp(Mathf.Pow(blockPosition - player.position.y, 3), -0.5f, 0.5f), 2*VERTICAL_SPEED * Time.deltaTime), 1);
+			player.rotation = new Quaternion(0, 0, Mathf.Lerp(player.rotation.z, Mathf.Clamp(blockPosition - player.position.y, -MAX_TURNING_ANGLE, MAX_TURNING_ANGLE), TURNING_SPEED * Time.deltaTime), 1);
         } else {
-            player.rotation = new Quaternion(0, 0, 0, 1);
+            player.rotation = new Quaternion(0, 0, Mathf.Lerp(player.rotation.z, 0, TURNING_SPEED * Time.deltaTime), 1);
         }
 		if (player.rotation.z != 0) {
-            player.position = Vector3.Lerp(player.position, new Vector3(player.position.x, blockPosition, 0), (0.5f + Mathf.Abs(player.rotation.z)) * VERTICAL_SPEED * Time.deltaTime);
+            player.position = Vector3.Lerp(player.position, new Vector3(player.position.x, blockPosition, 0), VERTICAL_SPEED * Time.deltaTime);
 		}
 	}
 
