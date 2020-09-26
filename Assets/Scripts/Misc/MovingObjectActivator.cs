e3e06cbf754c5c03ccb14db1545f5a090ef1c692
Guilherme de Deus <kingbowserii@gmail.com>
2020-09-26 17:05:22 -0300

    Activating movement in objects

---
+++ Assets/Scripts/Misc/MovingObjectActivator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObjectActivator : MonoBehaviour
+{
+    private bool shouldActivate = true;
+    public bool ShouldActivate { get => shouldActivate; set {
+            shouldActivate = value;
+            Debug.Log(shouldActivate);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (shouldActivate) {
+            GetComponent<DirectionalMovingObject>().enabled = true;
+            GetComponent<RotatingObject>().enabled = true;
+            GetComponent<AudioSource>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
