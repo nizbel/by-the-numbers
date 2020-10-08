5f09290ca392e98254ee771c5ea3cdf3839a27ab
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-08 02:27:41 -0300

    Adding more utils

---
+++ Assets/Scripts/Utils/TimeUtil.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TimeUtil  {
+
+    public static int ConvertTimestampToSeconds(string timestamp) {
+        string[] timestampParts = timestamp.Split(':');
+        return int.Parse(timestampParts[0]) * 60 + int.Parse(timestampParts[1]);
+    }
+}
