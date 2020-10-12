f28be812cab7bf4f4a142ca9e50a573411be59c7
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-12 01:30:11 -0300

    Adding random quaternion to utils

--- Assets/Scripts/Utils/GameObjectUtil.cs
+++ Assets/Scripts/Utils/GameObjectUtil.cs
@@ -9,4 +9,9 @@ public static class GameObjectUtil
 		// TODO find a way to get the object's largest sprite
 		return gameObj.GetComponent<SpriteRenderer>().sprite.bounds.extents.y * 2 * gameObj.transform.localScale.y;
 	}
+
+	public static Quaternion GenerateRandomRotation() {
+		return Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+
+	}
 }
