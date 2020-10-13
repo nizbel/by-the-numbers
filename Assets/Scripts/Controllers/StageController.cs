c443a85c27190e6fc160b8c85bb1111151865f84
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-13 20:53:52 -0300

    Removing commented code

--- Assets/Scripts/Controllers/StageController.cs
+++ Assets/Scripts/Controllers/StageController.cs
@@ -48,11 +48,6 @@ public abstract class StageController : MonoBehaviour {
 	protected bool rangeChangerWarned = false;
 	protected bool nextRangeChangerPositive;
 
-	// Stage events
-	//protected List<StageEvent> startingEventsList = new List<StageEvent>();
-	//protected List<StageEvent> gameplayEventsList = new List<StageEvent>();
-	//protected List<StageEvent> endingEventsList = new List<StageEvent>();
-
 	// Current stage state
 	// TODO Remove serializeField
 	[SerializeField]
@@ -74,61 +69,6 @@ public abstract class StageController : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-
-    // Use this for initialization
-    //void Start() {
-    //	// Start narrator controller
-    //	NarratorController.controller.StartGame();
-
-    //	// Load data for the day
-    //	LoadCurrentDayData();
-
-    //	// Get player objects
-    //	playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-    //	playerShipScript = playerTransform.gameObject.GetComponent<PlayerShip>();
-    //	playerShipTransform = playerShipScript.transform;
-
-    //	// Get score object
-    //	scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<TextMesh>();
-
-    //	// Keep track for range changer spawning
-    //	lastRangeChangerSpawned = Time.timeSinceLevelLoad;
-    //	DefineRangeChangerSpawn();
-    //	nextRangeChangerPositive = DefineNextRangeChangerType();
-    //}
-
-    // Update is called once per frame
-    //void Update() {
-    //	// Game Over
-    //	if ((playerShipScript.GetValue() < ValueRange.rangeController.GetMinValue()) ||
-    //		(playerShipScript.GetValue() > ValueRange.rangeController.GetMaxValue())) {
-    //		GameOver();
-    //	}
-
-    //	// Check if range changer can still spawn
-    //	if (state == GAMEPLAY_STATE) {
-    //		// Check if should warn about range changer
-    //		if (!rangeChangerWarned && Time.timeSinceLevelLoad - lastRangeChangerSpawned > currentRangeChangerSpawnTimer - WARNING_PERIOD_BEFORE_RANGE_CHANGER) {
-    //			WarnAboutRangeChanger();
-    //		}
-
-    //		// Check if range changer should be spawned
-    //		else if (Time.timeSinceLevelLoad - lastRangeChangerSpawned > currentRangeChangerSpawnTimer) {
-    //			GameObject newRangeChanger = (GameObject)Instantiate(rangeChangerPrefab, new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x + 2, 0, 0),
-    //																  transform.rotation);
-    //			// Set whether it is positive
-    //			newRangeChanger.GetComponent<RangeChanger>().SetPositive(nextRangeChangerPositive);
-
-    //			lastRangeChangerSpawned = Time.timeSinceLevelLoad;
-    //			DefineRangeChangerSpawn();
-    //			nextRangeChangerPositive = DefineNextRangeChangerType();
-    //			rangeChangerWarned = false;
-    //		}
-    //	}
-
-    //	// Control stage events
-    //	ControlEvents();
-    //}
 
     // Method for game over
     public void GameOver() {
@@ -217,84 +157,6 @@ public abstract class StageController : MonoBehaviour {
 		InputController inputController = FindObjectOfType<InputController>();
 		inputController.enabled = true;
 	}
-
-	//private void ControlEvents() {
-	//	// Check if current event is still valid
-	//	if (Time.time > currentEvent.GetStartTime() + currentEvent.GetDurationInSeconds()) {
-	//		// Check which list has the next event
-	//		if (startingEventsList.Count > 0) {
-	//			LoadCurrentEvent(startingEventsList);
-	//		}
-	//		else if (gameplayEventsList.Count > 0) {
-	//			if (state == STARTING_STATE) {
-	//				state = GAMEPLAY_STATE;
-	//				ScreenFadeController.controller.StartFadeIn();
-	//			}
-	//			LoadCurrentEvent(gameplayEventsList);
-	//		}
-	//		else if (endingEventsList.Count > 0) {
-	//			// Call fade out as soon as ending starts
-	//			if (state == GAMEPLAY_STATE) {
-	//				state = ENDING_STATE;
-	//				ScreenFadeController.controller.StartFadeOut();
-	//			}
-	//			LoadCurrentEvent(endingEventsList);
-	//		}
-	//		else {
-	//			// Day over (Story mode)
-	//			NarratorController.controller.GameOver();
-	//			// TODO Add day calculator object
-	//			if (GameController.controller.GetCurrentDay() == 1) {
-	//				GameController.controller.SetCurrentDay(2);
-	//				GameController.controller.ChangeState(GameController.GAMEPLAY_STORY);
-	//			} else {
-	//				GameController.controller.ChangeState(GameController.GAME_OVER_STORY);
-	//			}
-	//		}
-	//	}
-	//}
-
-	// Set day info for story mode
-	//public void LoadCurrentDayData() {
-	//	// Get current day
-	//	int currentDay = GameController.controller.GetCurrentDay();
-
-	//	// Load data from JSON
-	//	LoadEvents(currentDay);
-
-	//	// Prepare charges
-	//	currentSpecialCharges = currentDay * 4;
-	//}
-
-	//private void LoadEvents(int currentDay) {
-	//	var jsonFileStageParts = Resources.Load<TextAsset>(PATH_JSON_EVENTS + currentDay + "/starting");
-	//	startingEventsList.AddRange(JsonUtil.FromJson<StageEvent>(jsonFileStageParts.text));
-
-	//	jsonFileStageParts = Resources.Load<TextAsset>(PATH_JSON_EVENTS + currentDay + "/gameplay");
-	//	gameplayEventsList.AddRange(JsonUtil.FromJson<StageEvent>(jsonFileStageParts.text));
-
-	//	jsonFileStageParts = Resources.Load<TextAsset>(PATH_JSON_EVENTS + currentDay + "/ending");
-	//	endingEventsList.AddRange(JsonUtil.FromJson<StageEvent>(jsonFileStageParts.text));
-
-	//	LoadCurrentEvent(startingEventsList);
-	//}
-
-	//private void LoadCurrentEvent(List<StageEvent> eventList) {
-	//	// Set current event as first of the list
-	//	currentEvent = eventList[0];
-
-	//	// Remove it from list
-	//	eventList.RemoveAt(0);
-
-	//	// Set event's start time
-	//	currentEvent.SetStartTime(Time.time);
-	//	currentEvent.CalculateDurationInSeconds();
-
-	//	// If event has speech, pass it to Narrator Controller
-	//	if (currentEvent.speeches.Count > 0) {
-	//		NarratorController.controller.StartEventSpeech(currentEvent.speeches[0]);
-	//	}
-	//}
 
 	public void UseSpecialCharges(int chargesAmount) {
 		currentSpecialCharges -= chargesAmount;
