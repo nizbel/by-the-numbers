d8088161b4eaea82d74c4959aa7a6f7bef2cef63
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-14 22:41:52 -0300

    Adding the element to control foreground movement

---
+++ Assets/ForegroundLayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundLayer : MonoBehaviour
+{
+    float playerSpeed = 0;
+
+    bool currentLayer = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StageController.controller.AddForegroundLayer(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -100 && currentLayer) {
+            GameObject newLayerObject = new GameObject("Foreground Layer");
+            ForegroundLayer foregroundLayerScript = newLayerObject.AddComponent<ForegroundLayer>();
+
+            StageController.controller.AddForegroundLayer(foregroundLayerScript);
+
+            // Mark itself as not the current layer anymore
+            currentLayer = false;
+        } else if (!currentLayer && transform.childCount == 0) {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    void FixedUpdate() {
+        if (playerSpeed == 0) {
+            playerSpeed = StageController.controller.GetPlayerShipSpeed();
+        }
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - playerSpeed,
+                                                                          transform.position.y, transform.position.z), Time.deltaTime);
+    }
+}
