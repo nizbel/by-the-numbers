9dd73d2e1d6a17b7cd2a4eadaa3647b8774a17e3
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-17 14:45:34 -0300

    Defining  day choice

--- Assets/Scripts/Controllers/CurrentDayController.cs
+++ Assets/Scripts/Controllers/CurrentDayController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CurrentDayController : MonoBehaviour
 {
+    //private List<int> daysAvailable = new List<int> {
+    //    1, 2, 5, 6, 7, 8, 9, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 
+    //    29, 30, 31, 32, 35, 37, 38, 39, 41, 44, 46, 47, 48, 49, 50, 52, 53, 54, 
+    //    55, 56, 58, 59, 60, 62, 63, 64, 67, 68, 69, 70, 72, 74, 75, 78, 79, 81, 
+    //    82, 83, 84, 85, 87, 88, 89, 90 };
+
+    private List<int> daysAvailable = new List<int> {
+        1, 2, 5};
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +23,25 @@ public class CurrentDayController : MonoBehaviour
     void DefineNextDay() {
         int currentDay = GameController.controller.GetCurrentDay();
 
-        // TODO Define a routine to choose the next day
-        int nextDay = currentDay + 1;
+        int nextDay = 0;
+        // TODO Check if current day has restrictions
+        if (2 != 2) { }
+
+        else {
+            nextDay = GetPossibleNextDays(currentDay)[0];
+        }
         GameController.controller.SetCurrentDay(nextDay);
 
         Destroy(this);
+    }
+
+    private List<int> GetPossibleNextDays(int currentDay) {
+        List<int> possibleDays = new List<int>();
+
+        int currentDayIndex = daysAvailable.IndexOf(currentDay);
+
+        possibleDays.AddRange(daysAvailable.GetRange(currentDayIndex + 1, 6));
+
+        return possibleDays;
     }
 }
