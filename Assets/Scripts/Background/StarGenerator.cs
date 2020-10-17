440332cd7302bcf3a2e4074ae5f64fcd84447ec6
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-17 13:30:51 -0300

    Randomizing star generation

--- Assets/Scripts/Background/StarGenerator.cs
+++ Assets/Scripts/Background/StarGenerator.cs
@@ -1,65 +1,82 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class StarGenerator : BackgroundElementGenerator {
 
 	public const float MIN_STAR_GENERATION_PERIOD = 0.2f;
-	public const float MAX_STAR_GENERATION_PERIOD = 0.5f;
+	public const float MAX_STAR_GENERATION_PERIOD = 1.8f;
 	public const float MIN_STAR_SCALE = 0.03f;
 	public const float MAX_STAR_SCALE = 0.12f;
 
-	// Use this for initialization
-	void Start () {
+	public const int MAX_STAR_AMOUNT = 85;
+
+    private const float BASE_CHANCE = 25;
+    private const float BASE_CHANCE_MULTIPLIER = 0.475f;
+    private const float CHANCE_DIFF_MULTIPLIER = 4.5f;
+
+    // Use this for initialization
+    void Start () {
 		// Set values
 		minGenerationPeriod = MIN_STAR_GENERATION_PERIOD;
 		maxGenerationPeriod = MAX_STAR_GENERATION_PERIOD;
 		minElementScale = MIN_STAR_SCALE;
 		maxElementScale = MAX_STAR_SCALE;
 
-		DefineNextGeneration();
-		for (float startingStarPosition = GameController.GetCameraXMin() - 1; startingStarPosition < GameController.GetCameraXMax() + 1;) {
-			int i = Random.Range(0, prefabs.Length);
-			Vector3 objectPosition = new Vector3(startingStarPosition, 
-				Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0);
-			float objectScale = GenerateNormalDistributionScale();
+        maxAmount = Random.Range(30, MAX_STAR_AMOUNT + 1);
+
+        DefineNextGeneration();
+
+        while (GameController.RollChance(CalculateGeneratingChance())) {
+            int i = Random.Range(0, prefabs.Length);
+
+            Vector3 objectPosition = new Vector3(Random.Range(GameController.GetCameraXMin(), GameController.GetCameraXMax()),
+                Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0);
+            float objectScale = GenerateNormalDistributionScale();
             GameObject newObject = (GameObject)Instantiate(prefabs[i], objectPosition, Quaternion.Euler(0, 0, Random.Range(0, 180)));
-			newObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
-			
-			newObject.transform.parent = BackgroundStateController.controller.GetRandomBackgroundLayer().transform;
-			LayeredBackgroundObject layerScript = newObject.AddComponent<LayeredBackgroundObject>();
+            newObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
 
-			// Set this as its generator
-			newObject.GetComponent<GeneratedDestructible>().setGenerator(this);
-			IncreaseAmountAlive();
+            newObject.transform.parent = BackgroundStateController.controller.GetRandomBackgroundLayer().transform;
+            LayeredBackgroundObject layerScript = newObject.AddComponent<LayeredBackgroundObject>();
 
-			startingStarPosition += Random.Range(-0.1f, 0.5f);
+            // Set this as its generator
+            newObject.GetComponent<GeneratedDestructible>().setGenerator(this);
+            IncreaseAmountAlive();
+        }
 
-			// Check for stars in the static layer outside the screen
-			if (layerScript.CheckIfStaticLayer() && OutScreenDestroyerController.controller.ObjectCrossedCameraXBound(newObject)) {
-				Destroy(newObject);
-            }
-
-		}
-		// Update last generation variable
-		lastGeneratedTime = Time.timeSinceLevelLoad;
+        // Update last generation variable
+        lastGeneratedTime = Time.timeSinceLevelLoad;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.timeSinceLevelLoad - lastGeneratedTime > nextGeneration) {
-			// Choose prefab
-			int i = Random.Range(0, prefabs.Length);
+			if (GameController.RollChance(CalculateGeneratingChance())) {
+				if (Time.timeSinceLevelLoad - lastGeneratedTime > nextGeneration) {
+					// Choose prefab
+					int i = Random.Range(0, prefabs.Length);
 
-			Vector3 objectPosition = GenerateRandomPosition();
-			float objectScale = GenerateNormalDistributionScale();
+					Vector3 objectPosition = GenerateRandomPosition();
+					float objectScale = GenerateRandomScale();
 
-			GenerateNewObject(prefabs[i], objectPosition, objectScale);
+					GenerateNewObject(prefabs[i], objectPosition, objectScale);
 
-			// Update generation variables
-			lastGeneratedTime = Time.timeSinceLevelLoad;
+					// Update generation variables
+					lastGeneratedTime = Time.timeSinceLevelLoad;
+					DefineNextGeneration();
+				}
+			}
+
 			DefineNextGeneration();
 		}
-
 	}
+
+    private float CalculateGeneratingChance() {
+        if (maxAmount - amountAlive > 5) {
+            return 100;
+        }
+        else {
+            int targetAmountDiff = amountAlive - maxAmount;
+            return BASE_CHANCE - targetAmountDiff*CHANCE_DIFF_MULTIPLIER + Mathf.Pow(BASE_CHANCE_MULTIPLIER, targetAmountDiff);
+        }
+    }
 
 }
