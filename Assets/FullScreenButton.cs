30c24b8bd59d0e0dfce28f688edff905c9aa408a
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-20 21:07:52 -0300

    Allowing full screen and resolution changing

---
+++ Assets/FullScreenButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FullScreenButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Toggle>().isOn = Screen.fullScreen;
+
+        // Apply listener
+        GetComponent<Toggle>().onValueChanged.AddListener((value) => {
+            Screen.fullScreen = value;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ResolutionSlider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (Screen.currentResolution.width) {
+            case 1920:
+                GetComponent<Slider>().value = 2;
+                break;
+
+            case 1366:
+                GetComponent<Slider>().value = 1;
+                break;
+        }
+
+        // Apply listener for NarratorController
+        GetComponent<Slider>().onValueChanged.AddListener((value) => {
+            switch (value) {
+                case 1:
+                    Screen.SetResolution(1366, 768, Screen.fullScreen);
+                    break;
+
+                case 2:
+                    Screen.SetResolution(GameController.WINDOWS_RES_X, GameController.WINDOWS_RES_Y, Screen.fullScreen);
+                    break;
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
