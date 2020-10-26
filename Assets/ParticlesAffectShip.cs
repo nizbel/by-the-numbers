8b21a452f3d546787aa94b2b833dbe7a613bd213
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-25 23:51:32 -0300

    Particles affecting ship state

---
+++ Assets/ParticlesAffectShip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesAffectShip : MonoBehaviour
+{
+    private int value = 0;
+
+    public int Value { get => value; set => this.value = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO Add play sound for energy latching
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnParticleSystemStopped() {
+        PlayerController.controller.UpdateShipValue(value);
+        Destroy(gameObject);
+    }
+}
