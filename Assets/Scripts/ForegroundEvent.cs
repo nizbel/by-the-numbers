e9530b7100378248ac95752d099104bcb2366a1f
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-26 21:18:38 -0300

    Adding foreground event data to events

---
+++ Assets/Scripts/ForegroundEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundEvent : MonoBehaviour
+{
+    [SerializeField]
+    int chargesCost = 1;
+
+    [SerializeField]
+    float cooldown = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     * Getters and Setters
+     */
+    public int GetChargesCost() {
+        return chargesCost;
+    }
+
+    public void SetChargesCost(int chargesCost) {
+        this.chargesCost = chargesCost;
+    }
+
+    public float GetCooldown() {
+        return cooldown;
+    }
+    public void SetCooldown(float cooldown) {
+        this.cooldown = cooldown;
+    }
+}
