d185dd8010f358d5aed97528990242e472151bfd
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-27 21:37:41 -0300

    Making event choice more flexible

--- Assets/Scripts/ForegroundEventGenerator.cs
+++ Assets/Scripts/ForegroundEventGenerator.cs
@@ -7,15 +7,28 @@ public class ForegroundEventGenerator : MonoBehaviour
 	/* 
 	 * Constants
 	 */
-	public const float DEFAULT_MAX_EVENT_SPAWN_CHANCE = 50;
+	public const float DEFAULT_MIN_SPAWN_INTERVAL = 0.4f;
+	public const float DEFAULT_MAX_SPAWN_INTERVAL = 0.6f;
 
+	private const int ENERGY_FORMATION_TYPE = 1;
+	private const int OBSTACLE_GENERATOR_TYPE = 2;
+	private const int TURBINE_GROUP_TYPE = 3;
+	
+	public const float DEFAULT_MAX_EVENT_SPAWN_CHANCE = 50;
+	public const int DEFAULT_ENERGY_FORMATION_SPAWN_CHANCE = 65;
+	public const int DEFAULT_OBSTACLE_GENERATOR_SPAWN_CHANCE = 30;
+	public const int DEFAULT_TURBINE_GROUP_SPAWN_CHANCE = 5;
+
+	private int energyFormationChance = DEFAULT_ENERGY_FORMATION_SPAWN_CHANCE;
+	private int obstacleGeneratorChance = DEFAULT_OBSTACLE_GENERATOR_SPAWN_CHANCE;
+	private int turbineGroupSpawnChance = DEFAULT_TURBINE_GROUP_SPAWN_CHANCE;
+
+	// Keeps (type, chance) list
+	private List<(int, int)> spawnChancePool = new List<(int,int)>();
 	/*
 	 * Energy formation prefabs
 	 */
 	public List<GameObject> energyFormationList;
-	//private bool formationSpawned = false;
-	//private float formationCooldown = 0;
-	//private float lastEventSpawnTime = 0;
 
 	/*
 	 * Obstacle generator prefabs
@@ -25,7 +38,7 @@ public class ForegroundEventGenerator : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
-
+		PrepareChancesPool();
 	}
 
     // Update is called once per frame
@@ -36,11 +49,23 @@ public class ForegroundEventGenerator : MonoBehaviour
 
     // Spawn
     public void SpawnEvent() {
+		// TODO check if events have representants
+		int completeChance = spawnChancePool[spawnChancePool.Count-1].Item2;
+
+		int randomChoice = Mathf.RoundToInt(1 + Random.Range(0, 1.0f) * (completeChance - 1));
+
+		int type = GetTypeFromPoolChance(randomChoice);
+
+		Debug.Log(string.Join(",", spawnChancePool));
+		Debug.Log(type + "..." + randomChoice);
+
+		bool eventSpawned = false;
+
 		// TODO Improve choosing formation
 		// Check if spawned will be a formation or obstacle generator
-		if (GameController.RollChance(65) || StageController.controller.GetCurrentSpecialCharges() < 3) {
+		if (type == ENERGY_FORMATION_TYPE) {
 			// Define current spawning position
-			float curSpawnPosition = ForegroundElementGenerator.SPAWN_CAMERA_OFFSET + GameController.GetCameraXMax();
+			float curSpawnPosition = ForegroundController.SPAWN_CAMERA_OFFSET + GameController.GetCameraXMax();
 
 			GameObject energyFormation = energyFormationList[Random.Range(0, energyFormationList.Count)];
 			//GameObject energyFormation = energyFormationList[2];
@@ -50,16 +75,14 @@ public class ForegroundEventGenerator : MonoBehaviour
 				new Vector3(curSpawnPosition + formationScreenOffset, Random.Range(-1, 1), 0),
 				GameObjectUtil.GenerateRandomRotation());
 
-			// TODO check if spawned to count on stage's special spawning charges
+			// Check if spawned to count on stage's special spawning charges
 			if (spawnedFormation.Item1) {
-				//formationSpawned = true;
 				ForegroundController.controller.EventSpawned(spawnedFormation.Item2.GetComponent<ForegroundEvent>());
-				//formationCooldown = 0.1f * spawnedFormation.Item2.transform.childCount;
-				//lastEventSpawnTime = Time.time;
 				//Debug.Log(currentSpecialSpawnChance);
+				eventSpawned = true;
 			}
 		}
-		else {
+		else if (type == OBSTACLE_GENERATOR_TYPE) {
 			// Spawn obstacle generator
 			GameObject obstacleGenerator = obstacleGeneratorPrefabList[Random.Range(0, obstacleGeneratorPrefabList.Count)];
 
@@ -72,11 +95,8 @@ public class ForegroundEventGenerator : MonoBehaviour
 			(bool, GameObject) spawnedGeneration = SpawnForegroundElement(obstacleGenerator,
 				spawnPosition, new Quaternion(0, 0, 0, 1), false);
 
-			// TODO check if spawned to count on stage's special spawning charges
+			// Check if spawned to count on stage's special spawning charges
 			if (spawnedGeneration.Item1) {
-				//StageController.controller.UseSpecialCharges(3);
-				//lastEventSpawnTime = Time.time;
-
 				// Add duration to generator
 				TimedDurationObject durationScript = spawnedGeneration.Item2.AddComponent<TimedDurationObject>();
 				durationScript.Duration = 5;
@@ -88,8 +108,20 @@ public class ForegroundEventGenerator : MonoBehaviour
 				// Play warning on panel
 				StageController.controller.PanelWarnDanger();
 
+				ForegroundController.controller.EventSpawned(spawnedGeneration.Item2.GetComponent<ForegroundEvent>());
+
 				//Debug.Log(currentSpecialSpawnChance);
+				eventSpawned = true;
 			}
+		}
+
+		// If an event has really spawned, recalculate pool chances
+		if (eventSpawned) {
+			// Remove elements that have a cost above what's available
+			RemoveUnavailableEvents();
+
+			// Prepare chances
+			PrepareChancesPool();
 		}
 	}
 
@@ -121,5 +153,52 @@ public class ForegroundEventGenerator : MonoBehaviour
 		}
 
 		return (true, newForegroundElement);
+	}
+
+	void PrepareChancesPool() {
+		spawnChancePool.Clear();
+		// Energy formation
+		if (energyFormationList.Count > 0) {
+			AddChanceToPool(ENERGY_FORMATION_TYPE, energyFormationChance);
+		}
+
+		// Obstacle generator
+		if (obstacleGeneratorPrefabList.Count > 0) {
+			AddChanceToPool(OBSTACLE_GENERATOR_TYPE, obstacleGeneratorChance);
+		}
+	}
+
+	void AddChanceToPool(int spawnType, int chance) {
+		if (spawnChancePool.Count > 0) {
+			spawnChancePool.Add((spawnType, spawnChancePool[spawnChancePool.Count - 1].Item2 + chance));
+		}
+		else {
+			spawnChancePool.Add((spawnType, chance));
+		}
+	}
+
+	// Returns the type chosen based on the chance
+	int GetTypeFromPoolChance(int chance) {
+		foreach ((int, int) spawnChance in spawnChancePool) {
+			if (spawnChance.Item2 > chance) {
+				return spawnChance.Item1;
+            }
+        }
+		return 0;
+    }
+
+	void RemoveUnavailableEvents() {
+		RemoveEventsWithImpossibleCost(energyFormationList);
+		RemoveEventsWithImpossibleCost(obstacleGeneratorPrefabList);
+	}
+
+	void RemoveEventsWithImpossibleCost(List<GameObject> eventsList) {
+		for (int i = eventsList.Count - 1; i >= 0; i--) {
+			ForegroundEvent currentEvent = eventsList[i].GetComponent<ForegroundEvent>();
+
+			if (currentEvent.GetChargesCost() > StageController.controller.GetCurrentSpecialCharges()) {
+				eventsList.RemoveAt(i);
+			}
+		}
 	}
 }
