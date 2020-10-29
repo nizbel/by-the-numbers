00f0c1e6af96740d1167763161e591e0126bbdf6
Guilherme de Deus <kingbowserii@gmail.com>
2020-10-28 22:15:25 -0300

    Adding random size at awake to objects in the foreground

---
+++ Assets/Scripts/RandomSize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RandomSize : MonoBehaviour
+{
+    private const float MAX_SCALE = 1f;
+    private const float MIN_SCALE = 0.75f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        float randomScale = Random.Range(MIN_SCALE, MAX_SCALE);
+        transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+        Destroy(this);
+    }
+}
