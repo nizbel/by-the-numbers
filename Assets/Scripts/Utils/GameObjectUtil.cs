2dfec06f080279508aaf6442438541f00b25084c
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-01 18:54:24 -0300

    Adding half height calculation

--- Assets/Scripts/Utils/GameObjectUtil.cs
+++ Assets/Scripts/Utils/GameObjectUtil.cs
@@ -5,7 +5,13 @@ public static class GameObjectUtil
 
 	public static  float GetGameObjectVerticalSize(GameObject gameObj) {
 		// TODO find a way to get the object's largest sprite
-		return gameObj.GetComponent<SpriteRenderer>().sprite.bounds.extents.y * 2 * gameObj.transform.localScale.y;
+		//return gameObj.GetComponent<SpriteRenderer>().sprite.bounds.extents.y * 2 * gameObj.transform.localScale.y;
+		return GetGameObjectHalfVerticalSize(gameObj) * 2;
+	}
+
+	public static float GetGameObjectHalfVerticalSize(GameObject gameObj) {
+		// TODO find a way to get the object's largest sprite
+		return gameObj.GetComponent<SpriteRenderer>().sprite.bounds.extents.y * gameObj.transform.localScale.y;
 	}
 
 	public static Quaternion GenerateRandomRotation() {
