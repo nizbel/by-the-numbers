722a8bdf2815af2f492eec4a920762fa028984a8
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-02 14:25:57 -0300

    Adding parameters to random size

--- Assets/Scripts/RandomSize.cs
+++ Assets/Scripts/RandomSize.cs
@@ -2,13 +2,18 @@
 
 public class RandomSize : MonoBehaviour
 {
-    private const float MAX_SCALE = 1f;
-    private const float MIN_SCALE = 0.75f;
+    private const float DEFAULT_MAX_SCALE = 1f;
+    private const float DEFAULT_MIN_SCALE = 0.75f;
+
+    [SerializeField]
+    private float minScale = DEFAULT_MIN_SCALE;
+    [SerializeField]
+    private float maxScale = DEFAULT_MAX_SCALE;
 
     // Start is called before the first frame update
     void Awake()
     {
-        float randomScale = Random.Range(MIN_SCALE, MAX_SCALE);
+        float randomScale = Random.Range(minScale, maxScale);
         transform.localScale = new Vector3(randomScale, randomScale, randomScale);
         Destroy(this);
     }
