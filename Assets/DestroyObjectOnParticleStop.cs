c211946020694c07d16e7da8fac5e8729f42593d
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-02 15:01:05 -0300

    Allowing destructible gameobject to be set on script

--- Assets/DestroyObjectOnParticleStop.cs
+++ Assets/DestroyObjectOnParticleStop.cs
@@ -22,4 +22,8 @@ public class DestroyObjectOnParticleStop : MonoBehaviour
     public void OnParticleSystemStopped() {
         Destroy(gameObjectToDestroy);
     }
+
+    public void SetGameObjectToDestroy(GameObject gameObjectToDestroy) {
+        this.gameObjectToDestroy = gameObjectToDestroy;
+    }
 }
