23201485225b0c1263d7dd33b9b276ed6014f159
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-07 18:36:26 -0300

    Allowing player to leave mid-game

---
+++ Assets/Scripts/Menu/QuitRunButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuitRunButton : MonoBehaviour {
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void QuitRun() {
+		StageController.controller.ResumeGame();
+
+		// Disable narrator controller
+		NarratorController.controller.GameOver();
+
+		// Save data if story mode
+		if (GameController.controller.GetState() == GameController.GAMEPLAY_STORY) {
+			// TODO Save with unsuccesful times
+			StageInfo stageInfo = GameController.GetGameInfo().GetStageInfoByDay(GameController.controller.GetCurrentDay());
+			stageInfo.played = true;
+			GameController.controller.Save();
+		}
+
+		GameController.controller.ChangeState(GameController.MAIN_MENU);
+	}
+}
