08e9d0844d36481ca83e2d40c28f0476785a812e
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-14 00:43:34 -0300

    Adding more info to bullet time activator

--- Assets/BulletTimeActivator.cs
+++ Assets/BulletTimeActivator.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class BulletTimeActivator : MonoBehaviour
 {
+    Vector3 startingPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<BoxCollider2D>().enabled) {
-            transform.position = Vector3.right * transform.position.x + Vector3.up * PlayerController.controller.transform.position.y;
-        } else if (!GetComponent<AudioSource>().isPlaying) {
-            gameObject.SetActive(false);
-        }
+        //if (GetComponent<BoxCollider2D>().enabled) {
+        //    transform.position = Vector3.right * transform.position.x + Vector3.up * PlayerController.controller.transform.position.y;
+        //} else if (!GetComponent<AudioSource>().isPlaying) {
+        //    gameObject.SetActive(false);
+        //}
     }
 
 	void OnTriggerEnter2D(Collider2D collider) {
@@ -25,7 +27,14 @@ public class BulletTimeActivator : MonoBehaviour
             PlayerController.controller.ActivateBulletTime();
             GetComponent<AudioSource>().Play();
             GetComponent<BoxCollider2D>().enabled = false;
-
         }
 	}
+
+    public void SetMoving(bool moving) {
+        if (moving) {
+            transform.localPosition = startingPosition + Vector3.left * 0.2f;
+        } else {
+            transform.localPosition = startingPosition;
+        }
+    }
 }
