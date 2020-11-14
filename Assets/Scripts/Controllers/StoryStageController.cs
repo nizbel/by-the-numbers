d62bbafa202cc2efefca775e371543e363279ef5
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-14 19:18:11 -0300

    Starting timescale at current scale

--- Assets/Scripts/Controllers/StoryStageController.cs
+++ Assets/Scripts/Controllers/StoryStageController.cs
@@ -58,7 +58,7 @@ public class StoryStageController : StageController {
 		} else {
 			if (gameOverTimer > 0) {
 				gameOverTimer -= Time.unscaledDeltaTime;
-				TimeController.controller.SetTimeScale(Mathf.Lerp(0.1f, 0.9f, gameOverTimer / GAME_OVER_DURATION));
+				TimeController.controller.SetTimeScale(Mathf.Lerp(0.1f, Time.timeScale, gameOverTimer / GAME_OVER_DURATION));
             } else {
 				GameOver();
             }
