3031ca3bec58f6cd6f5b48d00c01517f1ca7724c
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-14 17:39:10 -0300

    Allows for collision using new player prefab

---
+++ Assets/Scripts/SpaceshipCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipCollider : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Block") {
+			PlayerController.controller.BlockCollisionReaction(collider);
+		}
+		else if (collider.gameObject.tag == "Power Up") {
+			PlayerController.controller.PowerUpCollisionReaction(collider);
+		}
+		else if (collider.gameObject.tag == "Obstacle") {
+			PlayerController.controller.ObstacleCollisionReaction(collider);
+		}
+	}
+}
