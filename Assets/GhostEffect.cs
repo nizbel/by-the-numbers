b084e5e5829a16cc4ce1c0e7f30453bd5ec018db
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-14 19:17:24 -0300

    Making ghost effect act better upon leaving spaceship

---
+++ Assets/GhostEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEffect : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem ghostParticle;
+
+    void Awake() {
+        transform.rotation = PlayerController.controller.transform.rotation;
+        ParticleSystem.MainModule main = ghostParticle.main;
+        main.startRotation = -transform.rotation.z*2;
+        ghostParticle.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
