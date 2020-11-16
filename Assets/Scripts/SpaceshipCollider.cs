02e919d0bccb529c0f96bdc45c0f26f8bf488a1f
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-16 01:32:35 -0300

    Fixing mine collision

--- Assets/Scripts/SpaceshipCollider.cs
+++ Assets/Scripts/SpaceshipCollider.cs
@@ -22,8 +22,8 @@ public class SpaceshipCollider : MonoBehaviour {
 		}
 		else if (collider.gameObject.tag == "Obstacle") {
 			PlayerController.controller.ObstacleCollisionReaction(collider);
-		} else if (collider.gameObject.tag == "Obstacle") {
-			PlayerController.controller.ObstacleCollisionReaction(collider);
+		} else if (collider.gameObject.tag == "Mine") {
+			PlayerController.controller.MineCollisionReaction(collider);
 		}
 	}
 }
