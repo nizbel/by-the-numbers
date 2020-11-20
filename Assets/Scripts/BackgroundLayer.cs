e9a2ee12123212bafd42178714eade06a3a2dca4
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-20 18:07:07 -0300

    Stopping background movement during cutscenes

--- Assets/Scripts/BackgroundLayer.cs
+++ Assets/Scripts/BackgroundLayer.cs
@@ -15,8 +15,11 @@ public class BackgroundLayer : MonoBehaviour
     }
 
     void FixedUpdate() {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - speed,
+        // TODO Check if the same could be applied to ForegroundLayer
+        if (StageController.controller.GetCurrentMomentType() != StageMoment.TYPE_CUTSCENE) {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - speed,
                                                                           transform.position.y, transform.position.z), Time.deltaTime);
+        }
     }
 
     /*
