9e05addf7f9d64fe695554a85636f3f8a02ee936
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-20 18:07:32 -0300

    Fading UI elements with the new screen fading

---
+++ Assets/Scripts/FadingUIElement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FadingUIElement : MonoBehaviour {
+    private const int IMAGE_TYPE = 1;
+    private const int TEXT_TYPE = 2;
+
+    Image image = null;
+    Text text = null;
+
+    // Define which element to fade
+    int type;
+
+    // Start is called before the first frame update
+    void Start() {
+        if (GetComponent<Image>() != null) {
+            image = GetComponent<Image>();
+            type = IMAGE_TYPE;
+        } else if (GetComponent<Text>() != null) {
+            text = GetComponent<Text>();
+            type = TEXT_TYPE;
+        }
+
+        // Always start invisible
+        SetAlpha(0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void SetAlpha(float alpha) { 
+        Color color;
+        switch (type) {
+            case IMAGE_TYPE:
+                color = image.color;
+                color.a = alpha;
+                image.color = color;
+                break;
+            case TEXT_TYPE:
+                color = text.color;
+                color.a = alpha;
+                text.color = color;
+                break;
+        }
+    }
+}
