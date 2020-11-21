70dbefed524555b5b40c28b9265573bc6a787445
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-21 17:38:28 -0300

    Allowing background element quantity scaling by day

--- Assets/Scripts/Background/BackgroundDebrisGenerator.cs
+++ Assets/Scripts/Background/BackgroundDebrisGenerator.cs
@@ -7,7 +7,10 @@ public class BackgroundDebrisGenerator : BackgroundElementGenerator {
 	public const float MAX_DEBRIS_GENERATION_PERIOD = 1.5f;
 	public const float MIN_DEBRIS_SCALE = 0.1f;
 	public const float MAX_DEBRIS_SCALE = 0.9f;
-	public const int MAX_DEBRIS_AMOUNT = 50;
+	public const int MAX_DEBRIS_AMOUNT = 20;
+
+	public AnimationCurve minDebrisByDay;
+	public AnimationCurve maxDebrisByDay;
 
 	// Use this for initialization
 	void Start () {
@@ -18,27 +21,58 @@ public class BackgroundDebrisGenerator : BackgroundElementGenerator {
 		maxElementScale = MAX_DEBRIS_SCALE;
 
 		DefineNextGeneration();
-		DefineMaxAmount(MAX_DEBRIS_AMOUNT);
+
+		(int, int) minMaxDebrisAmount = CalculateDebrisAmountByDay(GameController.controller.GetCurrentDay());
+		DefineMaxAmount(minMaxDebrisAmount.Item2, minMaxDebrisAmount.Item1);
 
 		// TODO Find a way to generate debris and galaxies at start, but debris will depend on a curve that starts at 0 (day 1), raises and ends at 0 (day 90)
+		while (GameController.RollChance(CalculateGeneratingChance())) {
+			int i = Random.Range(0, prefabs.Length);
+
+			Vector3 objectPosition = new Vector3(Random.Range(GameController.GetCameraXMin(), GameController.GetCameraXMax()),
+				Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0);
+			float objectScale = GenerateRandomScale();
+			GameObject newObject = (GameObject)Instantiate(prefabs[i], objectPosition, Quaternion.Euler(0, 0, Random.Range(0, 180)));
+			newObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
+
+			newObject.transform.parent = BackgroundStateController.controller.GetFastestBackgroundLayer().transform;
+			LayeredBackgroundObject layerScript = newObject.AddComponent<LayeredBackgroundObject>();
+
+			// Set this as its generator
+			newObject.GetComponent<GeneratedDestructible>().setGenerator(this);
+			IncreaseAmountAlive();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (amountAlive < maxAmount) {
-			if (Time.timeSinceLevelLoad - lastGeneratedTime > nextGeneration) {
+		if (Time.timeSinceLevelLoad - lastGeneratedTime > nextGeneration) {
+			if (GameController.RollChance(CalculateGeneratingChance())) {
 				// Choose prefab
 				int i = Random.Range(0, prefabs.Length);
 
 				Vector3 objectPosition = GenerateRandomPosition();
 				float objectScale = GenerateRandomScale();
 
-				GenerateNewObject(prefabs[i], objectPosition, objectScale);
-
-				// Update generation variables
-				lastGeneratedTime = Time.timeSinceLevelLoad;
-				DefineNextGeneration();
+				GenerateNewObject(prefabs[i], objectPosition, objectScale, 3);
 			}
-		}		
+			// Update generation variables
+			lastGeneratedTime = Time.timeSinceLevelLoad;
+			DefineNextGeneration();
+		}
 	}
+
+	private float CalculateGeneratingChance() {
+		if (maxAmount > 0) {
+			return 100 - (amountAlive) * (100 / maxAmount);
+		}
+		return 0;
+	}
+
+	// Returns (min, max) int tuple
+	private (int, int) CalculateDebrisAmountByDay(int currentDay) {
+		(int, int) debrisByDay = ((int) (minDebrisByDay.Evaluate((float)currentDay / 90) * MAX_DEBRIS_AMOUNT), 
+			(int) (maxDebrisByDay.Evaluate((float)currentDay / 90) * MAX_DEBRIS_AMOUNT));
+		return debrisByDay;
+    }
 }
--- Assets/Scripts/Background/BackgroundElementGenerator.cs
+++ Assets/Scripts/Background/BackgroundElementGenerator.cs
@@ -37,8 +37,8 @@ public class BackgroundElementGenerator : MonoBehaviour {
 		nextGeneration = Random.Range(minGenerationPeriod, maxGenerationPeriod);
 	}
 
-	protected void DefineMaxAmount(int maxAmount) {
-		maxAmount = Random.Range(0, maxAmount);
+	protected void DefineMaxAmount(int maxAmount, int minAmount=0) {
+		this.maxAmount = Random.Range(minAmount, maxAmount+1);
 	}
 
 	protected float GenerateRandomScale() {
@@ -54,14 +54,19 @@ public class BackgroundElementGenerator : MonoBehaviour {
 			Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0);
 	}
 
-	protected void GenerateNewObject(GameObject prefab, Vector3 position, float scale) {
+	protected void GenerateNewObject(GameObject prefab, Vector3 position, float scale, int layer=-1) {
         // Generate object and avoid showing on screen yet
         GameObject newObject = (GameObject)Instantiate(prefab, position, Quaternion.Euler(0, 0, Random.Range(0, 180)));
 		newObject.transform.localScale = new Vector3(scale, scale, scale);
 		PositionObjectOffScreen(newObject);
 
 		// Add to background layer
-		newObject.transform.parent = BackgroundStateController.controller.GetRandomMovingBackgroundLayer().transform;
+		if (layer == -1) {
+			newObject.transform.parent = BackgroundStateController.controller.GetRandomMovingBackgroundLayer().transform;
+		} else {
+			// TODO Make layer selectable instead of fastest layer
+			newObject.transform.parent = BackgroundStateController.controller.GetFastestBackgroundLayer().transform;
+		}
 		newObject.AddComponent<LayeredBackgroundObject>();
 
 		// Set this as its generator
