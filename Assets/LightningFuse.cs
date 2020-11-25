586bbf08850ae2d6fbc5446e09d94cd5f0dd9225
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-25 02:08:26 -0300

    Added lightning fuse obstacle

---
+++ Assets/LightningFuse.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningFuse : MonoBehaviour
+{
+    private const float DEFAULT_CHARGING_TIME = 0.8f;
+
+    private const int IDLE = 0;
+    private const int ROTATING = 1;
+    private const int CHARGING = 2;
+    private const int FIRING = 3;
+
+    GameObject lightningPrefab;
+
+    float chargingTime = DEFAULT_CHARGING_TIME;
+
+    SuddenRotatingElement rotationScript;
+
+    int state = 1;
+
+    ParticleSystem chargeSignal;
+
+    Transform energyStrikeTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationScript = GetComponent<SuddenRotatingElement>();
+        energyStrikeTransform = transform.Find("Energy Strike");
+        chargeSignal = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state) {
+            case IDLE:
+                StartRotationCountdown();
+                break;
+
+            case ROTATING:
+                if (!rotationScript.enabled) {
+                    Charge();
+                }
+                break;
+
+            case CHARGING:
+                chargingTime -= Time.deltaTime;
+                if (chargingTime <= 0) {
+                    Fire();
+                    chargingTime = DEFAULT_CHARGING_TIME;
+                }
+                break;
+
+            case FIRING:
+                if (energyStrikeTransform.localScale.y < 9.95f) {
+                    energyStrikeTransform.localScale = Vector3.Lerp(energyStrikeTransform.localScale, new Vector3(1, 10, 1), Time.deltaTime * 10.5f);
+                } else {
+                    energyStrikeTransform.localScale = Vector3.Lerp(energyStrikeTransform.localScale, new Vector3(0, 10, 1), Time.deltaTime * 17.5f);
+                    if (energyStrikeTransform.localScale.x < 0.05f) {
+                        energyStrikeTransform.gameObject.SetActive(false);
+                        state = IDLE;
+                    }
+                }
+                break;
+        }
+    }
+
+    void Charge() {
+        // Launch lightning signal
+        // TODO Change charging signal color depending on energy
+        chargeSignal.Play();
+
+        state = CHARGING;
+    }
+
+    void Fire() {
+        // TODO Generate lightning ray
+        energyStrikeTransform.gameObject.SetActive(true);
+        energyStrikeTransform.localScale = Vector3.zero;
+
+        state = FIRING;
+    }
+
+    void StartRotationCountdown() {
+        rotationScript.enabled = true;
+        state = ROTATING;
+    }
+}
---
+++ Assets/SuddenRotatingElement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuddenRotatingElement : MonoBehaviour
+{
+    private const float MIN_ROTATION_WAIT = 0.4f;
+    private const float MAX_ROTATION_WAIT = 0.6f;
+
+    private const float DEFAULT_ROTATION_OFFSET = 30;
+    private const float DEFAULT_ROTATION_SPEED = 25;
+
+    float waitTime;
+
+    bool isRotating = false;
+
+    float rotationAngle;
+
+    float rotationSpeed;
+
+    float rotationOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnEnable() {
+        DefineWaitTime();
+        DefineRotationAngle();
+
+        rotationSpeed = DEFAULT_ROTATION_SPEED;
+        rotationOffset = DEFAULT_ROTATION_OFFSET;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waitTime -= Time.deltaTime;
+        if (waitTime <= 0 && !isRotating) {
+            isRotating = true;
+        }
+    }
+
+    void FixedUpdate() {
+        if (isRotating) {
+            float angleToMove;
+            if (rotationSpeed > 0) {
+                angleToMove = rotationAngle + rotationOffset;
+            } else {
+                angleToMove = (rotationAngle - rotationOffset + 360) % 360;
+            }
+            //transform.Rotate(0, 0, Mathf.Lerp(transform.rotation.eulerAngles.z, rotationAngle + rotationOffset, rotationSpeed * Time.deltaTime));
+            transform.Rotate(0, 0, rotationSpeed);
+
+            Debug.Log(rotationSpeed + "..." + transform.rotation.eulerAngles.z + "..." + angleToMove + "..." + rotationAngle);
+            if ((rotationSpeed > 0 && transform.rotation.eulerAngles.z > angleToMove) || (rotationSpeed < 0 && transform.rotation.eulerAngles.z < angleToMove)) {
+                rotationOffset *= 0.5f;
+                rotationSpeed *= -0.8f;
+            }
+
+            if (rotationOffset <= 0.1f) {
+                isRotating = false;
+                enabled = false;
+            }
+        }
+    }
+
+    public void RestartRotation() {
+        DefineWaitTime();
+    }
+
+    void DefineWaitTime() {
+        waitTime = Random.Range(MIN_ROTATION_WAIT, MAX_ROTATION_WAIT);
+    }
+
+    void DefineRotationAngle() {
+        rotationAngle = (transform.rotation.eulerAngles.z + Random.Range(1, 3) * 45) % 360;
+    }
+}
