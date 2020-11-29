734d8a0700395723d66164d89076fc919eaec7dc
Guilherme de Deus <kingbowserii@gmail.com>
2020-11-29 00:39:50 -0300

    Adding rigidbody functioning to moving object

--- Assets/Scripts/Misc/MovingObject.cs
+++ Assets/Scripts/Misc/MovingObject.cs
@@ -5,11 +5,12 @@ public class MovingObject : MonoBehaviour
     [SerializeField]
     Vector3 speed = Vector3.zero;
 
+    Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +20,12 @@ public class MovingObject : MonoBehaviour
     }
 
     void FixedUpdate() {
-        transform.position = Vector3.Lerp(transform.position, transform.position + Speed, Time.deltaTime);
+        if (rigidbody == null) {
+            transform.position = Vector3.Lerp(transform.position, transform.position + speed, Time.deltaTime);
+        } else {
+            //rigidbody.MovePosition(Vector3.Lerp(transform.position, transform.position + speed, Time.deltaTime));
+            rigidbody.AddForce(speed * rigidbody.mass, ForceMode2D.Force);
+        }
     }
 
     public Vector3 Speed { get => speed; set => speed = value; }
