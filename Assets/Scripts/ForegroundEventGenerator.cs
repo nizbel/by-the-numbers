43078549a3b28b79c3dba39bfa610a96ce54ac40
Guilherme de Deus <kingbowserii@gmail.com>
2020-12-14 00:37:50 -0300

    Removing commented code

--- Assets/Scripts/ForegroundEventGenerator.cs
+++ Assets/Scripts/ForegroundEventGenerator.cs
@@ -135,21 +135,6 @@ public class ForegroundEventGenerator : MonoBehaviour
 		GameObject newForegroundElement = (GameObject)Instantiate(foregroundPrefab, position, new Quaternion(0, 0, 0, 1));
 		newForegroundElement.transform.localRotation = rotation;
 
-		//// Check if bound overlap
-		//if (newForegroundElement.GetComponent<Formation>() != null) {
-		//	// Get children of formation
-		//	foreach (Transform child in newForegroundElement.transform) {
-		//		foreach (GameObject block in GameObject.FindGameObjectsWithTag("Block")) {
-		//			if (block.transform.parent != newForegroundElement.transform) {
-		//				if (child.GetComponent<Collider2D>().bounds.Intersects(block.GetComponent<Collider2D>().bounds)) {
-		//					Destroy(newForegroundElement);
-		//					return (false, null);
-		//				}
-		//			}
-		//		}
-		//	}
-		//}
-
 		return (true, newForegroundElement);
 	}
 
@@ -161,7 +146,8 @@ public class ForegroundEventGenerator : MonoBehaviour
 		}
 
 		// Obstacle generator
-		if (obstacleGeneratorPrefabList.Count > 0) {
+		// TODO Remove day 32 workaround
+		if (obstacleGeneratorPrefabList.Count > 0 && GameController.controller.GetCurrentDay() != 32) {
 			AddChanceToPool(OBSTACLE_GENERATOR_TYPE, obstacleGeneratorChance);
 		}
 	}
