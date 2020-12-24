f469a30e28d551ffbaa6a2f6640d8c47e5da1e73
Guilherme de Deus <kingbowserii@gmail.com>
2020-12-24 18:38:10 -0300

    Preparing object pooling

--- Assets/ObjectPool.cs
+++ Assets/ObjectPool.cs
@@ -4,31 +4,64 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    public const int POSITIVE_ENERGY = 1;
+    public const int NEGATIVE_ENERGY = 2;
+
+    [System.Serializable]
+    public class Pool {
+        public int type;
+        public GameObject prefab;
+        public int amount;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<int, Queue<GameObject>> poolDictionary;
+
     public static ObjectPool SharedInstance;
-    public List<GameObject> pooledObjects;
-    public GameObject objectToPool;
-    public int amountToPool;
 
     void Awake() {
         SharedInstance = this;
     }
 
     void Start() {
-        pooledObjects = new List<GameObject>();
-        GameObject tmp;
-        for (int i = 0; i < amountToPool; i++) {
-            tmp = Instantiate(objectToPool);
-            tmp.SetActive(false);
-            pooledObjects.Add(tmp);
+        poolDictionary = new Dictionary<int, Queue<GameObject>>();
+
+        foreach (Pool pool in pools) {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.amount; i++) {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                obj.transform.parent = transform;
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.type, objectPool);
         }
     }
 
-    public GameObject GetPooledObject() {
-        for (int i = 0; i < amountToPool; i++) {
-            if (!pooledObjects[i].activeInHierarchy) {
-                return pooledObjects[i];
-            }
-        }
-        return null;
+    public GameObject SpawnPooledObject(int type) {
+        GameObject spawnedObject = poolDictionary[type].Dequeue();
+        spawnedObject.SetActive(true);
+        spawnedObject.GetComponent<IPooledObject>().OnObjectSpawn();
+        return spawnedObject;
+    }
+
+    public GameObject SpawnPooledObject(int type, Vector3 position, Quaternion rotation) {
+        GameObject spawnedObject = poolDictionary[type].Dequeue();
+        
+        // Activate and call spawn method
+        spawnedObject.SetActive(true);
+        spawnedObject.GetComponent<IPooledObject>().OnObjectSpawn();
+
+        // Set transform position and rotation
+        spawnedObject.transform.position = position;
+        spawnedObject.transform.localRotation = rotation;
+
+        return spawnedObject;
+    }
+
+    public void ReturnPooledObject(int type, GameObject obj) {
+        poolDictionary[type].Enqueue(obj);
+        obj.transform.parent = transform;
     }
 }
