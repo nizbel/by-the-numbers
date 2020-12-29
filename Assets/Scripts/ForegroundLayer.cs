af4d9bc5f90709b18d69aa9a3ee78cce513490e0
Guilherme de Deus <kingbowserii@gmail.com>
2020-12-29 02:20:32 -0300

    Fixing foreground layer creation order and moving code from Update to FixedUpdate

--- Assets/Scripts/ForegroundLayer.cs
+++ Assets/Scripts/ForegroundLayer.cs
@@ -10,34 +10,27 @@ public class ForegroundLayer : MonoBehaviour
 
     bool currentLayer = true;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         StageController.controller.AddForegroundLayer(this);
         playerSpeed = PlayerController.controller.GetSpeed();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate() {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - playerSpeed,
+                                                                          transform.position.y, transform.position.z), Time.deltaTime);
+        
         // Destroy game object once it reaches its limit, creating another to replace
         if (transform.position.x <= HORIZONTAL_LIMIT && currentLayer) {
             GameObject newLayerObject = new GameObject("Foreground Layer");
             ForegroundLayer foregroundLayerScript = newLayerObject.AddComponent<ForegroundLayer>();
 
-            StageController.controller.AddForegroundLayer(foregroundLayerScript);
-
             // Mark itself as not the current layer anymore
             currentLayer = false;
-        } else if (!currentLayer && transform.childCount == 0) {
+        }
+        else if (!currentLayer && transform.childCount == 0) {
             Destroy(gameObject);
         }
-
-    }
-
-    void FixedUpdate() {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - playerSpeed,
-                                                                          transform.position.y, transform.position.z), Time.deltaTime);
     }
 
     public void SetPlayerSpeed(float playerSpeed) {
