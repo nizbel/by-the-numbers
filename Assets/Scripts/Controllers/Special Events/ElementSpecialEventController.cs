d50efcc664762e9966bd1886b6ec3deb654f5c88
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-17 18:01:38 -0300

    Subclassing from a general special event element

---
+++ Assets/Scripts/Controllers/Special Events/ElementSpecialEventController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementSpecialEventController : MonoBehaviour {
+
+    protected const int INITIAL_STATE = 0;
+    protected const int SEEN_STATE = 1;
+    protected const int PAST_STATE = 2;
+
+    protected int state = INITIAL_STATE;
+
+    protected float waitTime = 1;
+
+    protected GameObject observableElement = null;
+
+    protected ElementEncounterStageMoment stageMoment = null;
+
+    protected int elementType;
+
+    // Speeches
+    protected Speech observeSpeech;
+    protected Speech aboutElementSpeech;
+
+    // Update is called once per frame
+    void Update() {
+        if (waitTime > 0) {
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0) {
+                SpawnElement();
+            }
+        }
+        else {
+            switch (state) {
+                case INITIAL_STATE:
+                    if (observableElement.transform.position.x < GameController.GetCameraXMax()) {
+                        ObserveElement();
+                    }
+                    break;
+                case SEEN_STATE:
+                    if (!observableElement.activeSelf) {
+                        SpeakAboutElement();
+                    }
+                    break;
+                case PAST_STATE:
+                    if (NarratorController.controller.GetState() != NarratorController.IMPORTANT) {
+                        // End moment
+                        EndStageMoment();
+
+                        // Save element as seen
+                        UpdateElementsSeen();
+
+                        Destroy(gameObject);
+                    }
+                    break;
+            }
+        }
+    }
+
+    protected virtual void SpawnElement() {
+    
+    }
+
+    protected void UpdateElementsSeen() {
+        GameController.GetGameInfo().elementsSeen[(int)stageMoment.element - 1] = true;
+        GameController.controller.Save();
+    }
+
+    protected void EndStageMoment() {
+        stageMoment.duration = "00:00";
+        stageMoment.CalculateDurationInSeconds();
+    }
+
+    protected void ObserveElement() {
+        // TODO Play observation remark
+        //NarratorController.controller.StartMomentSpeech(observeSpeech.ToString());
+
+        state = SEEN_STATE;
+    }
+
+    protected void SpeakAboutElement() {
+        // TODO Play about remark
+        //NarratorController.controller.StartMomentSpeech(aboutElementSpeech.ToString());
+
+        state = PAST_STATE;
+    }
+
+    public void SetStageMoment(ElementEncounterStageMoment stageMoment) {
+        this.stageMoment = stageMoment;
+    }
+}
--- Assets/Scripts/Controllers/Special Events/SpecialEventDebrisController.cs
+++ Assets/Scripts/Controllers/Special Events/SpecialEventDebrisController.cs
@@ -1,90 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class SpecialEventDebrisController : MonoBehaviour {
+public class SpecialEventDebrisController : ElementSpecialEventController {
 
-    private const int INITIAL_STATE = 0;
-    private const int SEEN_STATE = 1;
-    private const int PAST_STATE = 2;
+    private const int DEBRIS_AMOUNT = 4;
 
-    private int eventCode;
+    void Start() {
+        // Define element and speeches
+        elementType = ObjectPool.DEBRIS;
+        observeSpeech = new Speech();
+        aboutElementSpeech = new Speech();
+}
 
-    public int EventCode { get => eventCode; set => eventCode = value; }
+    protected override void SpawnElement() {
+        Vector2 position = new Vector2(GameController.GetCameraXMax() + 1, PlayerController.controller.transform.position.y);
+        observableElement = ObjectPool.SharedInstance.SpawnPooledObject(elementType, position, GameObjectUtil.GenerateRandomRotation());
 
-    private int state = INITIAL_STATE;
-
-    private float waitTime = 1;
-
-    private GameObject debris = null;
-
-    private ElementEncounterStageMoment stageMoment = null;
-
-    private void Start() {
-        // TODO Populate ElementEncounterStageMoment
-    }
-
-    // Update is called once per frame
-    void Update() {
-        if (waitTime > 0) {
-            waitTime -= Time.deltaTime;
-            if (waitTime <= 0) {
-                // Spawn debris
-                Vector2 position = new Vector2(GameController.GetCameraXMax() + 1, PlayerController.controller.transform.position.y);
-                debris = ObjectPool.SharedInstance.SpawnPooledObject(ObjectPool.DEBRIS, position, GameObjectUtil.GenerateRandomRotation());
-            }
+        // Spawn other debris
+        for (int i = 0; i < DEBRIS_AMOUNT; i++) {
+            position = new Vector2(GameController.GetCameraXMax() + Random.Range(1f, 3f), Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()));
+            ObjectPool.SharedInstance.SpawnPooledObject(elementType, position, GameObjectUtil.GenerateRandomRotation());
         }
-        else {
-            switch (state) {
-                case INITIAL_STATE:
-                    if (debris.transform.position.x < GameController.GetCameraXMax()) {
-                        ObserveDebris();
-                    }
-                    break;
-                case SEEN_STATE:
-                    if (!debris.activeSelf) {
-                        SpeakAboutElement();
-                    }
-                    break;
-                case PAST_STATE:
-                    if (NarratorController.controller.GetState() != NarratorController.IMPORTANT) {
-                        // End moment
-                        EndStageMoment();
-
-                        // Save element as seen
-                        UpdateElementsSeen();
-
-                        Destroy(gameObject);
-                    }
-                    break;
-            }
-        }
-    }
-
-    private void UpdateElementsSeen() {
-        GameController.GetGameInfo().elementsSeen[(int)stageMoment.element - 1] = true;
-        GameController.controller.Save();
-    }
-
-    private void EndStageMoment() {
-        stageMoment.duration = "00:00";
-        stageMoment.CalculateDurationInSeconds();
-    }
-
-    void ObserveDebris() {
-        // TODO Play observation remark
-        //NarratorController.controller.StartMomentSpeech("");
-
-        state = SEEN_STATE;
-    }
-
-    void SpeakAboutElement() {
-        // TODO Play observation remark
-        //NarratorController.controller.StartMomentSpeech("");
-
-        state = PAST_STATE;
-    }
-
-    public void SetStageMoment(ElementEncounterStageMoment stageMoment) {
-        this.stageMoment = stageMoment;
     }
 }
