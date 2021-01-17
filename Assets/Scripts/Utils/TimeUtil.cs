1bbc83892aa3799f8b410ec875716104119be733
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-16 23:49:19 -0300

    Adding conversion

--- Assets/Scripts/Utils/TimeUtil.cs
+++ Assets/Scripts/Utils/TimeUtil.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public static class TimeUtil  {
 
@@ -11,5 +12,11 @@ public static class TimeUtil  {
         } else {
             return int.Parse(timestampParts[0]) * 60 + int.Parse(timestampParts[1]);
         }
+    }
+    public static string ConvertSecondsToTimestamp(float duration) {
+        int minutes = (int) (duration / 60);
+        float seconds = duration % 60;
+
+        return String.Format("{0}:{1:00.#}", minutes, seconds);
     }
 }
