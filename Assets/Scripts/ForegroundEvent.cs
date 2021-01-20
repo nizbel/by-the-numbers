a13f1d838a6745d4f59221f4be4527661153a010
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-20 01:06:45 -0300

    Preparing mine event to not be a fixed prefab

--- Assets/Scripts/ForegroundEvent.cs
+++ Assets/Scripts/ForegroundEvent.cs
@@ -10,12 +10,6 @@ public class ForegroundEvent : MonoBehaviour
     [SerializeField]
     float cooldown = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     /*
      * Getters and Setters
      */
---
+++ Assets/Scripts/MineBurstEvent.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBurstEvent : ForegroundEvent {
+
+    // Constants
+    private const int MIN_ELEMENTS_NEARBY = 4;
+    private const int MAX_ELEMENTS_NEARBY = 8;
+    private const int TRIGGER_ELEMENT_CHANCE = 80;
+    private const int TRIGGER_ELEMENT_TRIGGER_CHANCE = 80;
+    private const float MIN_SQUARE_DISTANCE_BETWEEN_ELEMENTS = 1.2f;
+    private const float MIN_MINE_DISTANCE = 1.45f;
+
+    private float maxMineDistance = EnergyMine.EXPLOSION_RADIUS;
+
+    void Start() {
+        // Define what is around
+        DefineElementsAround();
+
+        // Define if an element is coming to hit
+        if (GameController.RollChance(TRIGGER_ELEMENT_CHANCE)) {
+            GameObject triggerElement = CreateTriggerElement();
+
+            MovingObject movingScript = triggerElement.AddComponent<MovingObject>();
+            // Define if element is going to hit
+            if (GameController.RollChance(TRIGGER_ELEMENT_TRIGGER_CHANCE)) {
+                // Element is headed towards mine
+                movingScript.Speed = (transform.position - triggerElement.transform.position);
+            }
+            else {
+                // Element will miss mine
+                movingScript.Speed = (transform.position + Vector3.up*2 - triggerElement.transform.position);
+            }
+        }
+    }
+
+    void DefineElementsAround() {
+        int numElements = Random.Range(MIN_ELEMENTS_NEARBY, MAX_ELEMENTS_NEARBY);
+        Vector3[] positions = new Vector3[numElements];
+        for (int i = 0; i < positions.Length; i++) {
+            positions[i] = transform.position;
+        }
+
+
+        for (int i = 0; i < numElements; i++) {
+            Vector3 newPosition = new Vector3();
+            int tries = 0;
+            do {
+                newPosition = GeneratePosition();
+                tries++;
+            } while (!CheckIfValidPosition(newPosition, positions) && tries < 3);
+
+            if (tries < 3) {
+                positions[i] = newPosition;
+                GenerateElement(newPosition);
+            }
+        }
+    }
+
+    Vector3 GeneratePosition() {
+        return transform.position + Quaternion.Euler(0, 0, Random.Range(0, 360)) * Vector3.right * Random.Range(MIN_MINE_DISTANCE, maxMineDistance);
+    }
+
+    bool CheckIfValidPosition(Vector3 position, Vector3[] positionsToTest) {
+        bool valid = true;
+        foreach (Vector3 positionToTest in positionsToTest) {
+            if ((position - positionToTest).sqrMagnitude < MIN_SQUARE_DISTANCE_BETWEEN_ELEMENTS) {
+                valid = false;
+                break;
+            }
+        }
+
+        return valid;
+    }
+
+    void GenerateElement(Vector3 position) {
+        int[] possibleElements = new int[] {
+            ObjectPool.POSITIVE_ENERGY,
+            ObjectPool.NEGATIVE_ENERGY,
+            ObjectPool.ASTEROID,
+            ObjectPool.DEBRIS,
+            ObjectPool.ENERGY_MINE};
+
+        ObjectPool.SharedInstance.SpawnPooledObject(possibleElements[Random.Range(0, possibleElements.Length - 1)], position, GameObjectUtil.GenerateRandomRotation());
+    }
+
+    GameObject CreateTriggerElement() {
+        int[] possibleElements = new int[] {
+            ObjectPool.POSITIVE_ENERGY,
+            ObjectPool.NEGATIVE_ENERGY,
+            ObjectPool.ASTEROID,
+            ObjectPool.DEBRIS};
+
+        return ObjectPool.SharedInstance.SpawnPooledObject(possibleElements[Random.Range(0, possibleElements.Length - 1)], 
+            transform.position + new Vector3(Random.Range(1.5f, 3), Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0), 
+            GameObjectUtil.GenerateRandomRotation());
+    }
+}
