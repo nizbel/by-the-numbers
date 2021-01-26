0a1e32f4ca06b8220a81b0a3732c95cfda058023
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-26 20:12:55 -0300

    Optimizing out of screen removal

--- Assets/Scripts/Controllers/OutScreenDestroyerController.cs
+++ Assets/Scripts/Controllers/OutScreenDestroyerController.cs
@@ -11,17 +11,17 @@ public class OutScreenDestroyerController : MonoBehaviour {
 	
 	public static OutScreenDestroyerController controller;
 
-	[SerializeField]
-	int randomTries = 1;
+	//[SerializeField]
+	//int randomTries = 1;
 
-	[SerializeField]
-	int randomDestroys = 0;
+	//[SerializeField]
+	//int randomDestroys = 0;
 
-	[SerializeField]
-	float averageDestroyTries = 0;
+	//[SerializeField]
+	//float averageDestroyTries = 0;
 
-	[SerializeField]
-	int objCount = 0;
+	//[SerializeField]
+	//int objCount = 0;
 
 	[SerializeField]
 	float currentLimit;
@@ -43,12 +43,18 @@ public class OutScreenDestroyerController : MonoBehaviour {
 		currentLimit = Random.Range(0.5f, 1);
 	}
 
+	float media = 0;
+	float qtd = 0;
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		/*
 		 * Pick one object at a serial order
 		 */
 		// Test if current index can be picked
+		//int quantidade = Mathf.FloorToInt(destructibleObjectsList.Count * currentLimit);
+		float inicio = Time.realtimeSinceStartup;
+
         currentCameraBorder = GameController.GetCameraXMin();
         while (true) {
             if (currentObjectIndex < Mathf.FloorToInt(destructibleObjectsList.Count * currentLimit)) {
@@ -59,15 +65,15 @@ public class OutScreenDestroyerController : MonoBehaviour {
 					destructibleObjectsList.Remove(curDestructible);
                     //Destroy(curDestructible.gameObject);
                     curDestructible.OnObjectDespawn();
-                    averageDestroyTries = ((averageDestroyTries * randomDestroys) + randomTries) / (randomDestroys + 1);
+                    //averageDestroyTries = ((averageDestroyTries * randomDestroys) + randomTries) / (randomDestroys + 1);
 					//				Debug.Log("Destroyed " + currentObjectIndex + " of " + destructibleObjectsList.Count + " with " + randomTries + " tries, average is " + averageDestroyTries + " tries");
-					randomTries = 1;
-					randomDestroys++;
+					//randomTries = 1;
+					//randomDestroys++;
 				}
 				else {
-					currentObjectIndex++;
-					randomTries++;
-				}
+                    currentObjectIndex++;
+                    //randomTries++;
+                }
 			}
 			// If can't, return to starting index
 			else {
@@ -82,7 +88,14 @@ public class OutScreenDestroyerController : MonoBehaviour {
             }
         }
 
-        objCount = destructibleObjectsList.Count;
+        //objCount = destructibleObjectsList.Count;
+
+		float resultado = (Time.realtimeSinceStartup - inicio);
+		//Debug.Log(resultado + " na media para Quantidade = " + quantidade);
+
+		media = (media * qtd + resultado) / (qtd + 1);
+		qtd++;
+		Debug.Log(media);
 	}
 	
 	public void AddToDestructibleList(DestructibleObject newDestructible) {
@@ -101,12 +114,15 @@ public class OutScreenDestroyerController : MonoBehaviour {
 		switch (destructible.GetDestructibleType()) {
 			case DestructibleObject.COMMON_SPRITE_TYPE:
 			case DestructibleObject.MULTIPLE_SPRITE_TYPE:
-				// Get bigger side of the sprite, the scale is always constant between x, y and z
-				float maxSideSprite = GameObjectUtil.GetBiggestSideOfSprite(destructible.GetSpriteRenderer().sprite);
+                if (destructible.transform.position.x < currentCameraBorder) {
+                    // Get bigger side of the sprite, the scale is always constant between x, y and z
+                    float maxSideSprite = GameObjectUtil.GetBiggestSideOfSprite(destructible.GetSpriteRenderer().sprite);
 
-                return maxSideSprite * destructible.transform.localScale.x
-                        + destructible.transform.position.x
-                        < currentCameraBorder || destructible.transform.position.sqrMagnitude > DEFAULT_SQR_DISTANCE_TO_DESTROY;
+                    return destructible.transform.position.x + maxSideSprite * destructible.transform.localScale.x < currentCameraBorder;
+                }
+                else {
+                    return destructible.transform.position.sqrMagnitude > DEFAULT_SQR_DISTANCE_TO_DESTROY;
+                }
 
             case DestructibleObject.FORMATION_TYPE:
 				return destructible.transform.childCount == 0;
