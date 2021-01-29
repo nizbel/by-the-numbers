6593c26395c120522a7c6f2ea6e52371afe64e68
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-29 01:16:50 -0300

    Adding enum for background layer choice

---
+++ Assets/Scripts/Enums/BackgroundLayerEnum.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[SerializeField]
+public enum BackgroundLayerEnum {
+    // Background
+    RandomBackgroundLayer,
+    RandomMovingBackgroundLayer,
+    SlowestBackgroundLayer,
+    FastestBackgroundLayer,
+
+    // Distant foreground
+    RandomDistantForegroundLayer,
+    SlowestDistantForegroundLayer,
+    FastestDistantForegroundLayer
+}
