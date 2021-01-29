6255715ee50ef7dc196209a6cf47e953508510a5
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-29 01:15:12 -0300

    Removing unused method

--- Assets/Scripts/SetDestructibleOnParticleStop.cs
+++ Assets/Scripts/SetDestructibleOnParticleStop.cs
@@ -6,12 +6,6 @@ public class SetDestructibleOnParticleStop : MonoBehaviour
 {
     DestructibleObject destructibleScript;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void OnParticleSystemStopped() {
         destructibleScript.SetIsDestructibleNow(true);
         // Destroy itself after job is done
