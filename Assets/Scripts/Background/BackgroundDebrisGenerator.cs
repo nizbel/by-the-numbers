0ca7da2c79578acd09147151f440a98e5123f22d
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-30 23:50:53 -0300

    Allowing debris to be generated like energies in foreground distant layers

--- Assets/Scripts/Background/BackgroundDebrisGenerator.cs
+++ Assets/Scripts/Background/BackgroundDebrisGenerator.cs
@@ -5,9 +5,8 @@ public class BackgroundDebrisGenerator : BackgroundElementGenerator {
 
 	public const float MIN_DEBRIS_GENERATION_PERIOD = 0.5f;
 	public const float MAX_DEBRIS_GENERATION_PERIOD = 1.5f;
-	public const float MIN_DEBRIS_SCALE = 0.1f;
-	public const float MAX_DEBRIS_SCALE = 0.9f;
 	public const int MAX_DEBRIS_AMOUNT = 20;
+	public const float MOVING_DEBRIS_CHANCE = 20;
 
 	public AnimationCurve minDebrisByDay;
 	public AnimationCurve maxDebrisByDay;
@@ -19,29 +18,23 @@ public class BackgroundDebrisGenerator : BackgroundElementGenerator {
 		// Set values
 		minGenerationPeriod = MIN_DEBRIS_GENERATION_PERIOD;
 		maxGenerationPeriod = MAX_DEBRIS_GENERATION_PERIOD;
-		minElementScale = MIN_DEBRIS_SCALE;
-		maxElementScale = MAX_DEBRIS_SCALE;
 
 		DefineNextGeneration();
 
+		// Define max debris amount depending on day
+		// TODO Apply the same for galaxies
 		(int, int) minMaxDebrisAmount = CalculateDebrisAmountByDay(GameController.controller.GetCurrentDay());
 		DefineMaxAmount(minMaxDebrisAmount.Item2, minMaxDebrisAmount.Item1);
 
-		// TODO Find a way to generate debris and galaxies at start, but debris will depend on a curve that starts at 0 (day 1), raises and ends at 0 (day 90)
-		while (GameController.RollChance(CalculateGeneratingChance())) {
-			Vector3 objectPosition = new Vector3(Random.Range(GameController.GetCameraXMin(), GameController.GetCameraXMax()),
-				Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0);
+		if (maxAmount > 0) {
+			while (GameController.RollChance(CalculateGeneratingChance())) {
+				Vector3 objectPosition = new Vector3(Random.Range(GameController.GetCameraXMin(), GameController.GetCameraXMax()),
+					Random.Range(GameController.GetCameraYMin(), GameController.GetCameraYMax()), 0);
 
-			float objectScale = GenerateRandomScale();
-			GameObject newObject = ObjectPool.SharedInstance.SpawnPooledObject(elementType, objectPosition, Quaternion.Euler(0, 0, Random.Range(0, 180)));
-			newObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
+				GameObject newObject = GenerateNewObject(objectPosition, 0, BackgroundLayerEnum.RandomDistantForegroundLayer, false);
+			}	
 
-			newObject.transform.parent = BackgroundStateController.controller.GetFastestBackgroundLayer().transform;
-			LayeredBackgroundObject layerScript = newObject.AddComponent<LayeredBackgroundObject>();
-
-			// Set this as its generator
-			newObject.GetComponent<GeneratedDestructible>().setGenerator(this);
-			IncreaseAmountAlive();
+			StartCoroutine(SpawnMovingDebris());
 		}
 	}
 
@@ -49,10 +42,7 @@ public class BackgroundDebrisGenerator : BackgroundElementGenerator {
 		while (StageController.controller.GetState() != StageController.ENDING_STATE) {
 			yield return new WaitForSeconds(nextGeneration);
 			if (GameController.RollChance(CalculateGeneratingChance())) {
-				Vector3 objectPosition = GenerateRandomPosition();
-				float objectScale = GenerateRandomScale();
-
-				GenerateNewObject(objectPosition, objectScale, 3);
+				GameObject newObject = GenerateNewObject(GenerateRandomPosition(), 0, BackgroundLayerEnum.RandomDistantForegroundLayer);
 			}
 			// Update generation variables
 			DefineNextGeneration();
@@ -61,7 +51,7 @@ public class BackgroundDebrisGenerator : BackgroundElementGenerator {
 
 	private float CalculateGeneratingChance() {
 		if (maxAmount > 0) {
-			return 100 - (amountAlive) * (100 / maxAmount);
+			return 100f * (maxAmount - amountAlive);
 		}
 		return 0;
 	}
