a5a4becb13a021ada9366fbbf4480fe8ff796035
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-31 14:27:59 -0300

    Allowing the ship to survive if far enough from the explosion, obstacles get damaged

--- Assets/Scripts/EnergyMine.cs
+++ Assets/Scripts/EnergyMine.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO Subclass from DestructibleObject in order to make spawn/despawn preparations
 public class EnergyMine : MonoBehaviour {
     // TODO Find a better way of making it proportional to force field radius
     public const float EXPLOSION_RADIUS = 2.5f;
+    public const float SQUARED_CORE_EXPLOSION_RADIUS = 4f;
 
     [SerializeField]
     [ColorUsage(true,true)]
@@ -60,12 +62,26 @@ public class EnergyMine : MonoBehaviour {
                 Vector3 distance = nearbyObject.transform.position - transform.position;
                 body2D.AddForce(distance * (EXPLOSION_RADIUS * EXPLOSION_RADIUS - distance.sqrMagnitude));
                 if (nearbyObject.tag == "Player") {
-                    // Player dies if too close to the blast
-                    StageController.controller.GetCurrentForegroundLayer().SetPlayerSpeed(0);
-                    StageController.controller.DestroyShip();
+                    Debug.Log(distance.sqrMagnitude);
+                    // Test if close enough to explode or just shake
+                    if (distance.sqrMagnitude <= SQUARED_CORE_EXPLOSION_RADIUS) {
+                        // Player dies if too close to the blast
+                        StageController.controller.GetCurrentForegroundLayer().SetPlayerSpeed(0);
+                        StageController.controller.DestroyShip();
+                    } else {
+                        // Camera shakes
+                        GameController.GetCamera().GetComponent<CameraShake>().DefaultShake();
+                        // TODO Add damage to the ship, move this to a player controlled class
+                        Material spaceshipMaterial = PlayerController.controller.GetSpaceshipSpriteRenderer().material;
+                        spaceshipMaterial.SetFloat("_CurrentDamage", Mathf.Min(spaceshipMaterial.GetFloat("_CurrentDamage") + 0.25f, 1));
+                        GameController.SetShipDamage(spaceshipMaterial.GetFloat("_CurrentDamage"));
+                    }
                 } else if (nearbyObject.tag == "Mine") {
                     // Mines should explode if too close to the blast
                     body2D.transform.parent.gameObject.GetComponent<EnergyMine>().Explode();
+                } else if (nearbyObject.tag == "Frail Obstacle") {
+                    DissolvingObject dissolveScript = body2D.gameObject.AddComponent<DissolvingObject>();
+                    dissolveScript.SetDissolutionByDamage();
                 }
             }
         }
