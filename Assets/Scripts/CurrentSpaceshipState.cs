27a2ce2b74e7f153a9a1262016f334c2c54a9fa8
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-31 14:24:10 -0300

    Parameterizing ship damage state

--- Assets/Scripts/CurrentSpaceshipState.cs
+++ Assets/Scripts/CurrentSpaceshipState.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CurrentSpaceshipState : MonoBehaviour
 {
+    public const int DAY_FOR_SECOND_SPRITE = 16;
+    public const int DAY_FOR_THIRD_SPRITE = 46;
+    public const int DAY_FOR_FOURTH_SPRITE = 81;
+
     [SerializeField]
     Sprite[] sprites;
 
@@ -24,24 +28,32 @@ public class CurrentSpaceshipState : MonoBehaviour
         // TODO Find a way to get current spaceship damage dynamically
 
         // TODO Alter these values
-        if (currentDay > 80) {
+        if (currentDay >= DAY_FOR_FOURTH_SPRITE) {
             spriteRenderer.sprite = sprites[3];
             spriteRenderer.material.SetFloat("_CurrentDamageSprite", 0);
             PlayerController.controller.GetBurningAnimationSpriteRenderer().sprite = sprites[3];
         }
-        else if (currentDay > 45) {
+        else if (currentDay >= DAY_FOR_THIRD_SPRITE) {
+            // Reset damage counter
+            if (currentDay == DAY_FOR_THIRD_SPRITE) {
+                GameController.SetShipDamage(0);
+            }
             spriteRenderer.sprite = sprites[2];
-            spriteRenderer.material.SetFloat("_CurrentDamage", (float)(currentDay - 46) / 35);
+            spriteRenderer.material.SetFloat("_CurrentDamage", Mathf.Max((float)(currentDay - DAY_FOR_THIRD_SPRITE) / (DAY_FOR_FOURTH_SPRITE - DAY_FOR_THIRD_SPRITE), GameController.GetShipDamage()));
             PlayerController.controller.GetBurningAnimationSpriteRenderer().sprite = sprites[2];
         }
-        else if (currentDay > 15) {
+        else if (currentDay >= DAY_FOR_SECOND_SPRITE) {
+            // Reset damage counter
+            if (currentDay == DAY_FOR_SECOND_SPRITE) {
+                GameController.SetShipDamage(0);
+            }
             spriteRenderer.sprite = sprites[1];
-            spriteRenderer.material.SetFloat("_CurrentDamage", (float)(currentDay - 16) / 30);
+            spriteRenderer.material.SetFloat("_CurrentDamage", Mathf.Max((float)(currentDay - DAY_FOR_SECOND_SPRITE) / (DAY_FOR_THIRD_SPRITE - DAY_FOR_SECOND_SPRITE), GameController.GetShipDamage()));
             PlayerController.controller.GetBurningAnimationSpriteRenderer().sprite = sprites[1];
         }
         else {
             // If none of the above, keep the default sprite in the sheet
-            spriteRenderer.material.SetFloat("_CurrentDamage", (float)(currentDay - 1) / 15);
+            spriteRenderer.material.SetFloat("_CurrentDamage", Mathf.Max((float)(currentDay - 1) / (DAY_FOR_SECOND_SPRITE-1), GameController.GetShipDamage()));
         }
 
         // Set seed for damage material
