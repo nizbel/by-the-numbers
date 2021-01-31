c4ec0a2139738bfc455db65088542fc2fbf41e30
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-30 23:42:25 -0300

    Setting sort and collision layers for distant foreground elements

--- Assets/Scripts/Background/LayeredBackgroundObject.cs
+++ Assets/Scripts/Background/LayeredBackgroundObject.cs
@@ -5,18 +5,54 @@ public class LayeredBackgroundObject : MonoBehaviour {
 
 	BackgroundLayer parentLayer;
 
+	bool isDistantForegroundLayer;
+
 	// Use this for initialization
 	void Start() {
 		parentLayer = transform.parent.GetComponent<BackgroundLayer>();
 		if (!CheckIfStaticLayer()) {
 			GetComponent<DestructibleObject>().SetSpeed(parentLayer.GetSpeed());
-		} else {
-			Destroy(GetComponent<DestructibleObject>());
-        }
-		Destroy(this);
-	}
 
-	public bool CheckIfStaticLayer() {
+			isDistantForegroundLayer = !BackgroundStateController.controller.IsBackgroundLayer(parentLayer.transform);
+
+			if (isDistantForegroundLayer) {
+				// Set collision layer
+				gameObject.layer = parentLayer.GetLayer();
+
+				// Set sprite sorting layers
+				if (GetComponent<MultipleSpriteObject>() != null) {
+					SpriteRenderer[] sprites = gameObject.GetComponentsInChildren<SpriteRenderer>();
+					foreach (SpriteRenderer sprite in sprites) {
+						sprite.sortingLayerName = parentLayer.GetSpriteLayerName();
+                    }
+                } else {
+					GetComponent<SpriteRenderer>().sortingLayerName = parentLayer.GetSpriteLayerName();
+				}
+				// If there are particle systems, apply rendering layer for them too
+				ParticleSystem[] particleSystems = gameObject.GetComponentsInChildren<ParticleSystem>();
+				foreach (ParticleSystem ps in particleSystems) {
+					ps.GetComponent<Renderer>().sortingLayerName = parentLayer.GetSpriteLayerName();
+				}
+
+				transform.localScale /= parentLayer.GetDistance();
+			}
+        } else {
+			Destroy(GetComponent<DestructibleObject>());
+			Destroy(this);
+		}
+
+    }
+
+    private void OnDisable() {
+		if (parentLayer != null) {
+			if (isDistantForegroundLayer) {
+				transform.localScale *= parentLayer.GetDistance();
+			}
+			Destroy(this);
+		}
+    }
+
+    public bool CheckIfStaticLayer() {
 		return parentLayer == null;
 	}
 }
