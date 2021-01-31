ce935bd5b0e3181463ce36b2e802b3bf87a17638
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-31 14:26:51 -0300

    Making forefront particles be dissolved

---
+++ Assets/Scripts/ChangeDissolveMaterialOnParticleStop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDissolveMaterialOnParticleStop : MonoBehaviour
+{
+    private const float MIN_DISSOLVE_AMOUNT = 0;
+    private const float MAX_DISSOLVE_AMOUNT = 0.9f;
+
+    Material material;
+
+    ParticleSystem particles;
+
+    private void Start() {
+        material = GetComponent<ParticleSystemRenderer>().material;
+        material.SetFloat("_DissolveAmount", Random.Range(MIN_DISSOLVE_AMOUNT, MAX_DISSOLVE_AMOUNT));
+
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    public void OnParticleSystemStopped() {
+        material.SetFloat("_DissolveAmount", Random.Range(MIN_DISSOLVE_AMOUNT, MAX_DISSOLVE_AMOUNT));
+        particles.Play();
+    }
+}
