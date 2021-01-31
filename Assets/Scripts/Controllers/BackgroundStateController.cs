e98182777cfae2dc302458b0dfd017c3e2b817c8
Guilherme de Deus <kingbowserii@gmail.com>
2021-01-30 23:33:22 -0300

    Removing layer and light finding that was hard coded. Layers are now sent as transforms. Added layers for foreground elements

--- Assets/Scripts/Controllers/BackgroundStateController.cs
+++ Assets/Scripts/Controllers/BackgroundStateController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine.Experimental.Rendering.Universal;
 
 public class BackgroundStateController : MonoBehaviour {
+	[SerializeField]
+	private Light2D globalLight;
 
-    // Controls the background layers that moves objects in the background
-    protected List<GameObject> backgroundLayers = new List<GameObject>();
+	[Header("Layers")]
+	[Tooltip("Controls the background layers that moves objects in the background")]
+	[SerializeField]
+	protected List<Transform> backgroundLayers = new List<Transform>();
 
-	private Light2D globalLight = null;
+	[Tooltip("Constrols the layers with foreground elements in the background")]
+	[SerializeField]
+	protected List<Transform> distantForegroundLayers = new List<Transform>();
 
+	[Header("Generators")]
 	[SerializeField]
 	StarGenerator starGenerator;
+
+    [SerializeField]
+    GalaxyGenerator galaxyGenerator;
+
+    [SerializeField]
+    BackgroundDebrisGenerator backgroundDebrisGenerator;
 
     public static BackgroundStateController controller;
 
@@ -18,18 +31,6 @@ public class BackgroundStateController : MonoBehaviour {
 		if (controller == null) {
 			controller = this;
 			
-			// Fill layers
-			foreach (Transform child in transform) {
-				if (child.name == "Layers") {
-					foreach (Transform layer in child) {
-						backgroundLayers.Add(layer.gameObject);
-                    }
-					break;
-                }
-			}
-
-			// Set background lighting
-			globalLight = GameObject.Find("Background Light").GetComponent<Light2D>();
 			globalLight.intensity = 0;
 		}
 		else {
@@ -41,30 +42,56 @@ public class BackgroundStateController : MonoBehaviour {
 		GetComponent<ConstellationController>().enabled = true;
 	}
 
-	public void AddBackgroundLayer(GameObject layer) {
+	/*
+	 * Background layer methods
+	 */
+	// TODO Check if should add layers later
+	public void AddBackgroundLayer(Transform layer) {
 		backgroundLayers.Add(layer);
-
 	}
 
-	public GameObject GetRandomBackgroundLayer() {
+	public Transform GetRandomBackgroundLayer() {
 		return backgroundLayers[Random.Range(0, backgroundLayers.Count)];
 	}
 
-	public GameObject GetStaticBackgroundLayer() {
+	public Transform GetStaticBackgroundLayer() {
 		return backgroundLayers[0];
     }
 
-    public GameObject GetRandomMovingBackgroundLayer() {
+    public Transform GetRandomMovingBackgroundLayer() {
         return backgroundLayers[Random.Range(1, backgroundLayers.Count)];
     }
 
-	public GameObject GetFastestBackgroundLayer() {
+	public Transform GetFastestBackgroundLayer() {
 		return backgroundLayers[backgroundLayers.Count - 1];
     }
-	public GameObject GetSlowestBackgroundLayer() {
+
+	public Transform GetSlowestBackgroundLayer() {
 		return backgroundLayers[1];
 	}
 
+	public bool IsBackgroundLayer(Transform layerTransform) {
+		return backgroundLayers.Contains(layerTransform);
+    }
+
+	/*
+	 * Distant foreground layer methods
+	 */
+	public Transform GetRandomDistantForegroundLayer() {
+		return distantForegroundLayers[Random.Range(0, distantForegroundLayers.Count)];
+	}
+
+	public Transform GetFastestDistantForegroundLayer() {
+		return distantForegroundLayers[distantForegroundLayers.Count - 1];
+	}
+
+	public Transform GetSlowestDistantForegroundLayer() {
+		return distantForegroundLayers[0];
+	}
+
+	/*
+	 * Background light methods
+	 */
 	public void IncreaseLight(float intensity) {
 		globalLight.intensity += intensity;
 		PlayerController.controller.SetLightIntensity(Mathf.Max(1 - globalLight.intensity, 0));
