0ebfd6024d21c0448d3bf797a10d2c1cc769c53e
Guilherme de Deus <kingbowserii@gmail.com>
2021-02-19 01:25:45 -0300

    Preparing events for day 13

---
+++ Assets/Scripts/Controllers/Special Events/SpecialEvent1ControllerDay13.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialEvent1ControllerDay13 : MonoBehaviour {
+
+    private int eventCode;
+
+    public int EventCode { get => eventCode; set => eventCode = value; }
+
+    /*
+	 * Meteor generator prefab
+	 */
+    public GameObject meteorGenerator;
+
+    // Use this for initialization
+    void Start() {
+        StartMeteorShower();
+        Destroy(gameObject);
+    }
+
+
+    void StartMeteorShower() {
+        // Spawn meteor generator
+        // Define a radial position from the middle horizontal right
+        float angle = Random.Range(-0.25f, 0.25f) * Mathf.PI;
+        float x = GameController.GetCameraXMax() * 1.25f + Mathf.Cos(angle) * GameController.GetCameraYMax();
+        float y = Mathf.Sin(angle) * GameController.GetCameraYMax();
+        Vector3 spawnPosition = new Vector3(x, y, 0);
+
+        // Spawn element
+        MeteorGenerator generator = GameObject.Instantiate(meteorGenerator, spawnPosition, Quaternion.identity).GetComponent<MeteorGenerator>();
+
+        // Add duration to generator
+        TimedDurationObject durationScript = generator.gameObject.AddComponent<TimedDurationObject>();
+        durationScript.Duration = StageController.controller.GetCurrentMomentDuration();
+        durationScript.WaitTime = ShowerEvent.SHOWER_WARNING_PERIOD;
+        durationScript.Activate();
+        // Make meteor generator activate after wait time
+        generator.enabled = false;
+        durationScript.AddOnWaitListener(generator.Enable);
+
+        // Play warning
+        StageController.controller.PanelWarnDanger();
+    }
+}
---
+++ Assets/Scripts/Controllers/Special Events/SpecialEvent2ControllerDay13.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialEvent2ControllerDay13 : MonoBehaviour {
+    /*
+     * Speeches
+     */
+    [SerializeField]
+    private Speech curiousFormationsSpeech;
+
+    private int eventCode;
+
+    public int EventCode { get => eventCode; set => eventCode = value; }
+
+    float duration;
+
+    /*
+	 * Energy wall prefab
+	 */
+    [SerializeField]
+    GameObject energyWallPrefab;
+
+    Coroutine generation;
+
+    // Use this for initialization
+    void Start() {
+        // Fill duration
+        duration = StageController.controller.GetCurrentMomentDuration();
+        StartCoroutine(PlayNarrator());
+        SpawnEnergyWall();
+
+        generation = StartCoroutine(GenerateWalls());
+    }
+
+    private void FixedUpdate() {
+        duration -= Time.fixedDeltaTime;
+        if (duration <= 0) {
+            StopCoroutine(generation);
+            Destroy(gameObject);
+        }
+    }
+
+    void SpawnEnergyWall() {
+        // Spawn walls composed by random energies
+        EnergyWall newEnergyWall = GameObject.Instantiate(energyWallPrefab).GetComponent<EnergyWall>();
+        newEnergyWall.SetMoving(GameController.RollChance(20));
+        newEnergyWall.SetType(EnergyWall.RANDOM_ENERGIES);
+    }
+
+    IEnumerator GenerateWalls() {
+        while (true) {
+            yield return new WaitForSeconds(Random.Range(ForegroundElementGenerator.DEFAULT_MIN_SPAWN_INTERVAL, ForegroundElementGenerator.DEFAULT_MIN_SPAWN_INTERVAL));
+            SpawnEnergyWall();
+        }
+    }
+
+    IEnumerator PlayNarrator() {
+        yield return new WaitForSeconds((duration/2) - 1);
+        NarratorController.controller.StartMomentSpeech(curiousFormationsSpeech);
+    }
+}
