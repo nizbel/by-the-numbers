fbd70e88ab3945a27b7844cb324dee29bb0e47cf
Guilherme de Deus <kingbowserii@gmail.com>
2021-03-03 19:59:28 -0300

    Adding stray engine effects script

---
+++ Assets/StrayEngineEffects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrayEngineEffects : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem engineFire;
+
+    [SerializeField]
+    ParticleSystem shockEffects;
+
+    public void StartShock() {
+        shockEffects.Play();
+    }
+
+    public void StartEngineFire(bool prewarm = false) {
+        ParticleSystem.MainModule main = engineFire.main;
+        main.prewarm = prewarm;
+        engineFire.Play();
+    }
+
+    // Sets if the engine is using positive or negative energy
+    public void SetEngineValue() {
+        // TODO Change effects' colors
+    }
+}
