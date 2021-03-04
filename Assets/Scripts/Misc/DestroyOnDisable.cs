233d382b002bb3107f9eb7d7455500e78a686663
Guilherme de Deus <kingbowserii@gmail.com>
2021-03-03 22:28:30 -0300

    Adding destroy on disable behavior to avoid energies with effects continuing after respawn

---
+++ Assets/Scripts/Misc/DestroyOnDisable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnDisable : MonoBehaviour
+{
+    private void OnDisable() {
+        Destroy(gameObject);
+    }
+}
