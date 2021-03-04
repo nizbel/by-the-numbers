5215a63d5b8ba7359a9294dacf6d03ae457b2e05
Guilherme de Deus <kingbowserii@gmail.com>
2021-03-03 23:46:15 -0300

    Adding variations to stray engine particle effects

--- Assets/StrayEngineEffects.cs
+++ Assets/StrayEngineEffects.cs
@@ -10,6 +10,12 @@ public class StrayEngineEffects : MonoBehaviour
     [SerializeField]
     ParticleSystem shockEffects;
 
+    [SerializeField]
+    ParticleSystem.MinMaxGradient positiveColor;
+
+    [SerializeField]
+    ParticleSystem.MinMaxGradient negativeColor;
+
     public void StartShock() {
         shockEffects.Play();
     }
@@ -21,7 +27,23 @@ public class StrayEngineEffects : MonoBehaviour
     }
 
     // Sets if the engine is using positive or negative energy
-    public void SetEngineValue() {
+    public void SetEngineValue(int value) {
         // TODO Change effects' colors
+        if (value > 0) {
+            ParticleSystem.ColorOverLifetimeModule colorLifetimeModule = engineFire.colorOverLifetime;
+            colorLifetimeModule.color = positiveColor;
+        } else if (value < 0) {
+            ParticleSystem.ColorOverLifetimeModule colorLifetimeModule = engineFire.colorOverLifetime;
+            colorLifetimeModule.color = negativeColor;
+
+            // TODO Find a way to change positive and negative shocks
+        } else {
+            Debug.LogError("VALUE CAN'T BE ZERO");
+        }
+    }
+
+    public void StopEffects() {
+        shockEffects.Stop();
+        engineFire.Stop();
     }
 }
